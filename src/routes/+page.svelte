431c2a443de79cdd67f6c16d1ea1530359708ed7
Bruno Prado <bwprado@gmail.com>
2023-08-13 11:49:20 -0300

    ♻️ Adds all inventory information

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,33 @@
 <script>
 	export let data;
-    const { inventory } = data
+	const { dashboard } = data;
 </script>
 
-<div class="w-1/2 flex flex-col gap-y-2 m-auto">
-	{#if inventory.length > 0}
-		{#each inventory as item}
-			<div>{item.product}</div>
-		{/each}
-	{:else}
-		<span>Nenhum produto encontrado</span>
-	{/if}
+<div class="w-1/2 flex flex-col gap-y-2 mx-auto p-8">
+	<table>
+		<thead>
+			<tr class="text-left">
+				<th>Produto</th>
+				<th>Quantidade</th>
+				<th>Preço médio</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#if dashboard.length > 0}
+				{#each dashboard as item}
+					<tr>
+						<td>{item.product_name}</td>
+						<td>{item.total_amount}</td>
+						<td
+							>{new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(
+								item.average_price
+							)}</td
+						>
+					</tr>
+				{/each}
+			{:else}
+				<span>Nenhum produto encontrado</span>
+			{/if}
+		</tbody>
+	</table>
 </div>
