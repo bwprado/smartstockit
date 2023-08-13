928d2aea4a06984b1b0272d095cc43d7fc2eeedb
Bruno Prado <bwprado@gmail.com>
2023-08-13 11:48:53 -0300

    💥 Makes all requests available to all pages

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,14 +3,19 @@ import { supabase } from "$lib/helpers/supabase"
 /** @type {import('./$types').PageServerLoad} */
 export const load = async () => {
 
-    const fetchData = async () => {
+    const fetchProductsData = async () => {
         const { data, error } = await supabase
             .from('products')
             .select()
         return error ? [] : data
     }
+    const fetchDashboardData = async () => {
+        const { data, error } = await supabase.rpc('get_product_summary')
 
+        return error ? [] : data
+    }
     return {
-        products: fetchData()
+        products: fetchProductsData(),
+        dashboard: fetchDashboardData()
     }
 }
