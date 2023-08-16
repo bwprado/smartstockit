3303cf93d0ad920a2490054fd4e430825ae6fe9e
Bruno Prado <bwprado@gmail.com>
2023-08-15 21:54:39 -0300

    ✨ Adds dedicated dashboard page

---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+	export let data;
+	const { dashboard } = data || {};
+</script>
+
+<div class="w-1/2 flex flex-col gap-y-2 mx-auto p-8">
+	<table>
+		<thead>
+			<tr class="text-left">
+				<th>Produto</th>
+				<th>Quantidade</th>
+				<th>Preço médio</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#if dashboard?.length > 0}
+				{#each dashboard as item}
+					<tr>
+						<td>{item.product_name}</td>
+						<td>{item.total_amount}</td>
+						<td
+							>{new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(
+								item.average_price
+							)}</td
+						>
+					</tr>
+				{/each}
+			{:else}
+				<span>Nenhum produto encontrado</span>
+			{/if}
+		</tbody>
+	</table>
+</div>
