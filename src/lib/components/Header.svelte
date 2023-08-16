d58dc19ddf88a0ef1ca8902ed27a4654a26e4f84
Bruno Prado <bwprado@gmail.com>
2023-08-15 21:50:51 -0300

    ✨ Adds new itens to the menu

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,7 @@
-<script>
+<script lang="ts">
 	import { page } from '$app/stores';
+	export let session: any;
+	console.log(session);
 </script>
 
 <header class="flex h-10 w-full bg-gray-800 items-center text-gray-200 justify-between p-4">
@@ -7,8 +9,8 @@
 
 	<nav class="align-middle">
 		<ul class="flex gap-x-6">
-			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Dashboard</a>
+			<li aria-current={$page.url.pathname === '/dashboard' ? 'page' : undefined}>
+				<a href="/dashboard">Dashboard</a>
 			</li>
 			<li aria-current={$page.url.pathname === '/products' ? 'page' : undefined}>
 				<a href="/products">Products</a>
@@ -18,6 +20,9 @@
 			</li>
 			<li aria-current={$page.url.pathname.startsWith('/outupt') ? 'page' : undefined}>
 				<a href="/output">Output</a>
+			</li>
+			<li aria-current={$page.url.pathname.startsWith('/login')}>
+				<a href={session ? '/logout' : '/login'}>{session ? 'Logout' : 'Login'}</a>
 			</li>
 		</ul>
 	</nav>
