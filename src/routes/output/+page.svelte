00e03a6dedd3485f49adc91adcf527f5995bd594
Bruno Prado <bwprado@gmail.com>
2023-08-15 21:57:46 -0300

    ♻️ Changes page and adds `averagePrice`

--- src/routes/output/+page.svelte
+++ src/routes/output/+page.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
+	import type { PageData } from './$types';
 	interface Dashboard {
-		product_name: string;
 		product_id: string;
-		total_amount: number;
+		product_name: string;
 		average_price: number;
+		total_amount: number;
 	}
 
-	export let data;
-	const { products } = data;
-	const dashboard = data?.dashboard as Dashboard[];
+	export let data: PageData;
+	const products = data?.products || [];
+	const dashboard: Dashboard[] = data?.dashboard || [];
 	let selectedProduct = '' as string;
+	const averagePrice =
+		dashboard.length > 0 &&
+		dashboard?.find((item) => item?.product_id === selectedProduct)?.average_price;
 </script>
 
 <div class="w-1/2 flex flex-col mx-auto">
@@ -37,11 +41,7 @@
 				class="[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
 			/>
 		</div>
-		<input
-			type="hidden"
-			name="price"
-			value={dashboard.find((item) => item.product_id === selectedProduct)?.average_price}
-		/>
+		<input type="hidden" name="price" value={averagePrice} />
 		<button type="submit" class="p-4 bg-red-500 rounded-lg text-gray-100 hover:opacity-50"
 			>Retirar do Estoque</button
 		>
