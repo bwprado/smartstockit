4b90eb1e46adcaf1cd17998efca1b62cf675bf46
Bruno Prado <bwprado@gmail.com>
2023-08-15 21:54:24 -0300

    ♻️ Changes `formData` and adds security check

--- src/routes/input/+page.server.ts
+++ src/routes/input/+page.server.ts
@@ -1,20 +1,17 @@
-import { supabase } from '$lib/helpers/supabase.js';
+import { redirect, type Actions } from "@sveltejs/kit";
+import type { PageServerLoad } from "./$types";
 
-export interface IProduct { product: string, amount: string, price: string, fresh?: string, expiration_date?: string }
+export const load: PageServerLoad = async ({ locals: { getSession } }) => {
+    if (!await getSession()) {
+        throw redirect(303, '/login')
+    }
+};
 
-/** @type {import('./$types').Actions} */
-export const actions = {
-    default: async ({ request }) => {
-        const data = await request.formData()
-        const allData = [...data.entries()]
-        let inputData: IProduct = { product: '', amount: '', price: '' }
-        type T = keyof typeof inputData
-
-        for (const [key, value] of allData) {
-            inputData[key as T] = value as string
-        }
+export const actions: Actions = {
+    default: async ({ request, locals: { supabase } }) => {
+        const inputData = Object.fromEntries(await request.formData())
 
         inputData?.fresh && delete inputData.fresh
-        const res = await supabase.from('inventory').insert([inputData])
+        await supabase.from('inventory').insert([inputData])
     }
 };
