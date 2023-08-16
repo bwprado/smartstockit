1f43eab0397bb462826f15bf4d30fa712cc9add4
Bruno Prado <bwprado@gmail.com>
2023-08-15 21:51:53 -0300

    💥 Changes to add `locals` and `session`

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,21 +1,24 @@
-import { supabase } from "$lib/helpers/supabase"
+import type { LayoutServerLoad } from "./$types"
 
-/** @type {import('./$types').PageServerLoad} */
-export const load = async () => {
-
+export const load: LayoutServerLoad = async ({ locals: { supabase, getSession } }) => {
+    const session = await getSession()
+    console.log(session)
     const fetchProductsData = async () => {
+        console.log('fetching products')
         const { data, error } = await supabase
             .from('products')
             .select()
         return error ? [] : data
     }
     const fetchDashboardData = async () => {
+        console.log('fetching summary')
         const { data, error } = await supabase.rpc('get_product_summary')
 
         return error ? [] : data
     }
     return {
         products: fetchProductsData(),
-        dashboard: fetchDashboardData()
+        dashboard: fetchDashboardData(),
+        session: getSession()
     }
 }
