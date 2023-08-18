d42fa67e324956f2a5ed22de1498c7070c6e9ea6
Bruno Prado <bwprado@gmail.com>
2023-08-17 23:59:10 -0300

    🏷️ Adds user profile types

--- src/app.d.ts
+++ src/app.d.ts
@@ -5,15 +5,23 @@ declare global {
         interface Locals {
             supabase: SupabaseClient
             getSession(): Promise<Session | null>
+            getUserProfile(): Promise<{ id: string, name: string, email: string, role: "user" | "admin" }>
             signIn(email: string, password: string): Promise<{ user: User, session: Session }>
             signOut(): void
         }
         interface PageData {
-            session: Session | null
+            session: Promise<Session | null>
+            user: Promise<{ id: string, name: string, email: string, role: "user" | "admin" }>
         }
 
         interface LayoutData {
-            session: Session | null
+            session: Promise<Session | null>
+            user: Promise<{ id: string, name: string, email: string, role: "user" | "admin" }>
+        }
+
+        interface LayoutServerLoad {
+            session: Promise<Session | null>
+            user: Promise<{ id: string, name: string, email: string, role: "user" | "admin" }>
         }
     }
 }
