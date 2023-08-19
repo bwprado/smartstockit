cc184769544b5cd410a7c0e59d8cdf5286e0bb5b
Bruno Prado <bwprado@gmail.com>
2023-08-19 13:28:08 -0300

    ♻️ Changes to `class` mode on `darkMode`

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    'src/**/*.{html,js,svelte}'],
-  // ...
+    darkMode: 'class',
+    content: [
+        'src/**/*.{html,js,svelte}'],
+    // ...
 }
