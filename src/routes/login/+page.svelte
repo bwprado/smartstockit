2d1f51f75ce494192c2ea1e73bd84a7491b6e23c
Bruno Prado <bwprado@gmail.com>
2023-08-19 13:30:41 -0300

    ♻️ Chages to new components

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,16 +1,12 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
+	import Button from '$lib/components/Button.svelte';
+	import Input from '$lib/components/Input.svelte';
 	export let form;
 </script>
 
 <form method="post" class="flex flex-col gap-y-4 pt-8">
-	<div class="flex flex-col gap-y-1">
-		<label for="email">Email</label>
-		<input type="email" name="email" id="email" />
-	</div>
-	<div class="flex flex-col gap-y-1">
-		<label for="password">Password</label>
-		<input type="password" name="password" id="password" />
-	</div>
-	<button type="submit" class="rounded-lg bg-blue-600 text-gray-100 font-bold p-3">Login</button>
+	<Input id="email" name="email" label="Email" type="text" />
+	<Input id="password" name="password" label="Password" type="password" />
+	<Button type="submit" id="login">Login</Button>
 </form>
