786e5b02779f2f85f6e8d1fb70998c3205bfd7f4
Bruno Prado <bwprado@gmail.com>
2023-08-19 13:30:31 -0300

    💬 Updates `index` text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,17 @@
 	<title>Inventory - VT</title>
 </svelte:head>
 
-<h1 class="pt-10 text-xxl font-bold">Seja bem vindo ao Inventory - VT</h1>
+<div class="h-full flex items-center justify-center text-gray-900 w-full">
+	<div class="text-center px-4 sm:px-0 lg:px-28">
+		<h1 class="text-6xl font-bold mb-6">Inventory - VT</h1>
+		<p class="text-2xl mb-8">
+			A ferramenta definitiva de gerenciamento de estoque para o seu negócio. Simplifique,
+			automatize e tenha controle total do seu estoque como nunca antes.
+		</p>
+		<a
+			href="/login"
+			class="bg-blue-600 text-white px-8 py-3 rounded-md hover:bg-blue-500 transition duration-300"
+			>Comece Agora</a
+		>
+	</div>
+</div>
