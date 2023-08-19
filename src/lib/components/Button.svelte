7c2d3ef9c54c28fbf86aa56f93143502a5874794
Bruno Prado <bwprado@gmail.com>
2023-08-19 13:28:40 -0300

    ✨ New `Button` component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	export let type: import('svelte/elements').HTMLButtonAttributes['type'] = 'button';
+	export let id: string = '';
+	export let onClick: () => void = () => {};
+	export let href: HTMLAnchorElement['href'] = '';
+</script>
+
+{#if href}
+	<a {href}>
+		<button
+			{type}
+			{id}
+			class="rounded-lg bg-blue-600 text-gray-100 font-bold p-3 flex items-center justify-center"
+			on:click={onClick}
+		>
+			<slot />
+		</button>
+	</a>
+{:else}
+	<button
+		type="submit"
+		class="rounded-lg bg-blue-600 text-gray-100 font-bold p-3 flex items-center justify-center"
+	>
+		<slot />
+	</button>
+{/if}
