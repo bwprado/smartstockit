976419950b09b5cbf9ce368680b7190a6251c447
Bruno Prado <bwprado@gmail.com>
2023-08-19 13:30:02 -0300

    ✨ New `Table` components for a custom `table` experience.

---
+++ src/lib/components/Table/Table.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	export let columns: string[] = [];
+</script>
+
+<div class="overflow-x-auto">
+	<table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+		<thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+			<tr>
+				{#each columns as column}
+					<th scope="col" class="px-6 py-3">{column}</th>
+				{/each}
+			</tr>
+		</thead>
+		<tbody>
+			<slot />
+		</tbody>
+	</table>
+</div>
