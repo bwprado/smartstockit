788cf92bb1e78fea9ed2934ce8d801b39aaf7fca
Bruno Prado <bwprado@gmail.com>
2023-08-20 12:32:59 -0300

    ✨ Adds `Lightswitch` to toggle dark/white mode

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-	export let session: any;
-	export let user: any;
+	import { page } from '$app/stores'
+	import { LightSwitch, modeCurrent, setModeCurrent } from '@skeletonlabs/skeleton'
+	export let session: any
+	export let user: any
 </script>
 
-<header class="flex h-max w-full bg-gray-800 items-center text-gray-200 justify-between p-4 lg:px-28">
-	<div class=""><a href="/">InventoryVT</a></div>
-
+<header
+	class="flex h-max w-full bg-gray-800 items-center text-gray-200 justify-between py-4 lg:px-28 px-4">
+	<div class="">
+		<a href="/">InventoryVT</a>
+	</div>
 	<nav class="align-middle">
 		<ul class="flex gap-x-6">
 			{#if session && user?.role === 'admin'}
@@ -29,6 +32,8 @@
 			<li aria-current={$page.url.pathname.startsWith('/login')}>
 				<a href={session ? '/logout' : '/login'}>{session ? 'Logout' : 'Login'}</a>
 			</li>
+
+			<LightSwitch on:click={() => setModeCurrent($modeCurrent ? true : false)} />
 		</ul>
 	</nav>
 </header>
