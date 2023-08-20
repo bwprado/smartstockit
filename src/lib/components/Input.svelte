62882608517cb0b9a7a6b2298cc8bb5bd74e2db5
Bruno Prado <bwprado@gmail.com>
2023-08-20 12:34:46 -0300

    🎨 Changes styles and adds `onInput`  custom event

--- src/lib/components/Input.svelte
+++ src/lib/components/Input.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
-	import { twMerge } from 'tailwind-merge';
-	import { cx } from 'class-variance-authority';
+	import { twMerge } from 'tailwind-merge'
+	import { cx } from 'class-variance-authority'
 
-	export let label: string = 'Label';
+	export let label: string = 'Label'
 	export let customClasses: {
-		wrapper?: string;
-		label?: string;
-		input?: string;
-	} = {};
-	export let type: string = 'text';
-	export let name: string = 'input';
-	export let id: string = 'input';
+		wrapper?: string
+		label?: string
+		input?: string
+	} = {}
+	export let type: string = 'text'
+	export let name: string = 'input'
+	export let id: string = 'input'
+	export let onInput: (e: Event) => void = () => {}
+
 	const inputStyle = cx([
 		'block',
 		'w-full',
@@ -18,7 +20,7 @@
 		'border-0',
 		'py-1.5',
 		'px-2',
-        'h-8',
+		'h-8',
 		'text-gray-900',
 		'ring-1',
 		'ring-inset',
@@ -29,15 +31,17 @@
 		'focus:ring-indigo-600',
 		'sm:text-sm',
 		'sm:leading-6',
-		'outline-none'
-	]);
+		'outline-none',
+		'[appearance:textfield]',
+		'[&::-webkit-outer-spin-button]:appearance-none',
+		'[&::-webkit-inner-spin-button]:appearance-none'
+	])
 </script>
 
 <div class={twMerge('w-full flex flex-col gap-y-1', customClasses.wrapper)}>
 	<label
 		for={name}
 		class={twMerge('block text-sm leading-6 text-gray-900 font-bold', customClasses.label)}
-		>{label}</label
-	>
-	<input {type} {name} {id} class={twMerge(inputStyle, customClasses.input)} />
+		>{label}</label>
+	<input {type} {name} {id} class={twMerge(inputStyle, customClasses.input)} on:input={onInput} />
 </div>
