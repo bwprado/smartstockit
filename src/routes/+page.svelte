3ebba8fda62b587eb2e06764d9cdc7996721bade
Bruno Prado <bwprado@gmail.com>
2023-08-20 12:36:35 -0300

    💄 Changes in styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	<title>Inventory - VT</title>
 </svelte:head>
 
-<div class="h-full flex items-center justify-center text-gray-900 w-full">
+<div class="flex items-center justify-center text-gray-900 w-full h-max">
 	<div class="text-center px-4 sm:px-0 lg:px-28">
 		<h1 class="text-6xl font-bold mb-6">Inventory - VT</h1>
 		<p class="text-2xl mb-8">
