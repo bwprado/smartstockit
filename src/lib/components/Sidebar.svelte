54dbd40952df91d17e1dcca996945d5f2cc326a1
Bruno Prado <bwprado@gmail.com>
2023-08-20 12:35:29 -0300

    ✨ New `Sidebar` component

---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { page } from '$app/stores'
+	import { cva } from 'class-variance-authority'
+
+	export let navItems: { label: string; link: string }[] = []
+	$: active = $page.url.pathname
+	const linkStyle = cva([], {
+		variants: {
+			active: {
+				true: 'font-bold',
+				false: 'font-medium'
+			}
+		}
+	})
+</script>
+
+<nav class="bg-gray-200 h-full w-36 my-auto p-4">
+	<ul class="flex flex-col gap-y-4">
+		{#each navItems as { link, label }}
+			<li class={linkStyle({ active: active === link })}>
+				<a href={link}>{label}</a>
+			</li>
+		{/each}
+	</ul>
+</nav>
