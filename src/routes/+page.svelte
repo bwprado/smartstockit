03eab7dea20e92cb575effc491f03aa943f43897
Bruno Prado <bwprado@gmail.com>
2023-08-20 23:47:14 -0300

    💄 Adds `dark` mode

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	<title>Inventory - VT</title>
 </svelte:head>
 
-<div class="flex items-center justify-center text-gray-900 w-full h-max">
+<div class="flex items-center justify-center text-gray-900 w-full h-max dark:text-gray-200">
 	<div class="text-center px-4 sm:px-0 lg:px-28">
 		<h1 class="text-6xl font-bold mb-6">Inventory - VT</h1>
 		<p class="text-2xl mb-8">
@@ -12,7 +12,6 @@
 		<a
 			href="/login"
 			class="bg-blue-600 text-white px-8 py-3 rounded-md hover:bg-blue-500 transition duration-300"
-			>Comece Agora</a
-		>
+			>Comece Agora</a>
 	</div>
 </div>
