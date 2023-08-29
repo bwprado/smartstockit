5d82b72d1ce4b40f3184d48f8a180b710b56a9aa
Bruno Prado <bwprado@gmail.com>
2023-08-29 11:08:00 -0300

    🎨 Prettier auto formatting

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,17 @@
-import adapter from '@sveltejs/adapter-vercel';
-import preprocess from 'svelte-preprocess';
+import adapter from "@sveltejs/adapter-vercel"
+import preprocess from "svelte-preprocess"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: [preprocess()],
     kit: {
-        adapter: adapter(),
+        adapter: adapter({
+            runtime: "nodejs18.x",
+        }),
         alias: {
-            '@components': '$lib/components',
-        }
-    }
-};
+            "@components": "$lib/components",
+        },
+    },
+}
 
-export default config;
+export default config
