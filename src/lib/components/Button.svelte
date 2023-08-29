251ec8753e900f47af99d83cde90b5848fdcf4c4
Bruno Prado <bwprado@gmail.com>
2023-08-29 11:58:23 -0300

    🎨 Adds `intent` and `cva` conditional styling

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,25 +1,33 @@
 <script lang="ts">
+    import { cva } from "class-variance-authority"
+
     export let type: import("svelte/elements").HTMLButtonAttributes["type"] = "button"
     export let id: string = ""
     export let href: HTMLAnchorElement["href"] = ""
+    export let intent: "primary" | "secondary" | "danger" = "primary"
+
+    const btnStyle = cva(
+        ["rounded-lg", "font-bold", "p-3", "text-sm", "flex", "items-center", "justify-center"],
+        {
+            variants: {
+                intent: {
+                    primary: ["bg-primary-500", "text-primary-50", "hover:bg-primary-700"],
+                    secondary: ["bg-transparent", "text-primary-50", "hover:bg-primary-500"],
+                    danger: ["bg-red-500", "text-gray-100", "hover:bg-red-700"],
+                },
+            },
+        },
+    )
 </script>
 
 {#if href}
     <a {href}>
-        <button
-            {type}
-            {id}
-            class="rounded-lg bg-primary-500 text-gray-100 font-bold p-3 text-sm flex items-center justify-center hover:bg-primary-700"
-        >
+        <button {type} {id} class={btnStyle({ intent })}>
             <slot />
         </button>
     </a>
 {:else}
-    <button
-        on:click
-        type="submit"
-        class="rounded-lg bg-primary-500 text-gray-100 font-bold p-3 flex text-sm items-center justify-center hover:bg-primary-700"
-    >
+    <button on:click type="submit" class={btnStyle({ intent })}>
         <slot />
     </button>
 {/if}
