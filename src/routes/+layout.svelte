2e236a590f20727a14d9994e4a447e6b22242d83
Bruno Prado <bwprado@gmail.com>
2023-08-29 09:58:55 -0300

    🎨 Prettier formatting and adds `Drawer` component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,44 +1,50 @@
 <script lang="ts">
-	import '@skeletonlabs/skeleton/themes/theme-crimson.css'
-	import '@skeletonlabs/skeleton/styles/skeleton.css'
+    import "../app.css"
+    import { page } from "$app/stores"
+    import { AppShell, Drawer } from "@skeletonlabs/skeleton"
+    import { initializeStores } from "@skeletonlabs/skeleton"
 
-	import { page } from '$app/stores'
-	import { AppShell } from '@skeletonlabs/skeleton'
+    initializeStores()
 
-	import Header from '$lib/components/Header.svelte'
-	import Breadcrumbs from '$lib/components/Breadcrumbs.svelte'
-	import Sidebar from '$lib/components/Sidebar.svelte'
+    import Header from "$lib/components/Header.svelte"
+    import Breadcrumbs from "$lib/components/Breadcrumbs.svelte"
+    import Sidebar from "$lib/components/Sidebar.svelte"
 
-	export let data
+    export let data
 </script>
 
 <svelte:head>
-	<title>Inventory VT {$page.url.pathname}</title>
+    <title>Inventory VT {$page.url.pathname}</title>
 </svelte:head>
 
-<AppShell>
-	<svelte:fragment slot="header">
-		<Header session={data?.session || false} user={data?.user} />
-	</svelte:fragment>
-	<Breadcrumbs />
-	<svelte:fragment slot="sidebarLeft">
-		<Sidebar
-			navItems={[
-				{ label: 'Home', link: '/' },
-				{ label: 'Produtos', link: '/products' },
-				{ label: 'Entradas', link: '/input' },
-				{ label: 'Saídas', link: '/output' },
-			]} />
-	</svelte:fragment>
+<Drawer />
 
-	<div class="px-4 lg:px-28">
-		<slot />
-	</div>
-	<svelte:fragment slot="pageFooter">
-		<footer class="flex justify-center items-center">
-			<p class="text-xl">
-				<a href="https://kit.svelte.dev">by Bruno Prado</a> trying to learn SvelteKit
-			</p>
-		</footer>
-	</svelte:fragment>
+<AppShell>
+    <svelte:fragment slot="header">
+        <Header session={data?.session || false} user={data?.user} />
+    </svelte:fragment>
+    <Breadcrumbs />
+    <svelte:fragment slot="sidebarLeft">
+        <Sidebar
+            navItems={[
+                { label: "Home", link: "/" },
+                { label: "Admin", link: "/admin" },
+                { label: "Dashboard", link: "/dashboard" },
+                { label: "Produtos", link: "/products" },
+                { label: "Entradas", link: "/input" },
+                { label: "Saídas", link: "/output" },
+            ]}
+        />
+    </svelte:fragment>
+
+    <div class="px-4 lg:px-28">
+        <slot />
+    </div>
+    <svelte:fragment slot="pageFooter">
+        <footer class="flex justify-center items-center">
+            <p class="text-xl">
+                <a href="https://kit.svelte.dev">by Bruno Prado</a> trying to learn SvelteKit
+            </p>
+        </footer>
+    </svelte:fragment>
 </AppShell>
