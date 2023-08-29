06588df2c0b2400d3edcf6a621843bbf88781e38
Bruno Prado <bwprado@gmail.com>
2023-08-29 09:58:03 -0300

    🎨Adds style

--- src/lib/components/Table/TableCellHead.svelte
+++ src/lib/components/Table/TableCellHead.svelte
@@ -1,3 +1,5 @@
-<th scope="row" class="px-6 py-4 font-bold text-gray-900 whitespace-nowrap dark:text-white">
-	<slot />
+<th
+    scope="row"
+    class="px-6 py-4 font-bold whitespace-nowrap table-cell">
+    <slot />
 </th>
