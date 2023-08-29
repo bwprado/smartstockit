4f619cdafeb994a7e7c5cad8a618e016c75e5cd8
Bruno Prado <bwprado@gmail.com>
2023-08-29 09:57:05 -0300

    🎨 Prettier formatting and dark mode

--- src/lib/components/Select.svelte
+++ src/lib/components/Select.svelte
@@ -1,48 +1,50 @@
 <script lang="ts">
-	import { twMerge } from 'tailwind-merge';
-	import { cx } from 'class-variance-authority';
+    import { twMerge } from "tailwind-merge"
+    import { cx } from "class-variance-authority"
 
-	export let label: string = 'Label';
-	export let customClasses: {
-		wrapper?: string;
-		label?: string;
-		select?: string;
-	} = {};
-	export let type: string = 'text';
-	export let name: string = 'input';
-	export let id: string = 'input';
-	export let options: string[] = [];
-	const selectStyle = cx([
-		'block',
-		'w-full',
-		'rounded-md',
-		'border-0',
-		'py-1.5',
-		'px-2',
-		'h-8',
-		'text-gray-900',
-		'ring-1',
-		'ring-inset',
-		'ring-gray-300',
-		'placeholder:text-gray-400',
-		'focus:ring-2',
-		'focus:ring-inset',
-		'focus:ring-indigo-600',
-		'sm:text-sm',
-		'sm:leading-6',
-		'outline-none'
-	]);
+    export let label: string = "Label"
+    export let customClasses: {
+        wrapper?: string
+        label?: string
+        select?: string
+    } = {}
+    export let type: string = "text"
+    export let name: string = "input"
+    export let id: string = "input"
+    export let options: { name: string; id: string }[] = []
+    const selectStyle = cx([
+        "block",
+        "w-full",
+        "rounded-md",
+        "border-0",
+        "py-1.5",
+        "px-2",
+        "h-10",
+        "text-gray-900",
+        "ring-1",
+        "ring-inset",
+        "ring-gray-300",
+        "placeholder:text-gray-400",
+        "focus:ring-2",
+        "focus:ring-inset",
+        "focus:ring-indigo-600",
+        "sm:text-sm",
+        "sm:leading-6",
+        "outline-none",
+    ])
 </script>
 
-<div class={twMerge('w-full flex flex-col py-4', customClasses.wrapper)}>
-	<label
-		for={name}
-		class={twMerge('block text-sm font-bold leading-6 text-gray-900', customClasses.label)}
-		>{label}</label
-	>
-	<select {type} {name} {id} class={twMerge(selectStyle, customClasses.select)}>
-		{#each options as option}
-			<option value={option}>{option}</option>
-		{/each}
-	</select>
+<div class={twMerge("w-full flex flex-col py-4", customClasses.wrapper)}>
+    <label
+        for={name}
+        class={twMerge(
+            "block text-sm font-bold leading-6 text-gray-900 dark:text-gray-200",
+            customClasses.label,
+        )}>{label}</label>
+    <select type={type} name={name} id={id} class={twMerge(selectStyle, customClasses.select)}>
+        <option value="" disabled selected> Selecione uma opção </option>
+        {#each options as option}
+            <option value={option.id}>{option.name}</option>
+        {/each}
+    </select>
 </div>
