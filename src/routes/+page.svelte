3e6d966404c0e7006bf6511b0a1a31be0ac78113
Bruno Prado <bwprado@gmail.com>
2023-08-29 09:59:09 -0300

    🎨 Prettier formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,18 @@
 <svelte:head>
-	<title>Inventory - VT</title>
+    <title>Inventory - VT</title>
 </svelte:head>
 
 <div class="flex items-center justify-center text-gray-900 w-full h-max dark:text-gray-200">
-	<div class="text-center px-4 sm:px-0 lg:px-28">
-		<h1 class="text-6xl font-bold mb-6">Inventory - VT</h1>
-		<p class="text-2xl mb-8">
-			A ferramenta definitiva de gerenciamento de estoque para o seu negócio. Simplifique,
-			automatize e tenha controle total do seu estoque como nunca antes.
-		</p>
-		<a
-			href="/login"
-			class="bg-blue-600 text-white px-8 py-3 rounded-md hover:bg-blue-500 transition duration-300"
-			>Comece Agora</a>
-	</div>
+    <div class="text-center px-4 sm:px-0 lg:px-28">
+        <h1 class="text-6xl font-bold mb-6">Inventory - VT</h1>
+        <p class="text-2xl mb-8">
+            A ferramenta definitiva de gerenciamento de estoque para o seu negócio. Simplifique,
+            automatize e tenha controle total do seu estoque como nunca antes.
+        </p>
+        <a
+            href="/login"
+            class="bg-blue-600 text-white px-8 py-3 rounded-md hover:bg-blue-500 transition duration-300"
+            >Comece Agora</a
+        >
+    </div>
 </div>
