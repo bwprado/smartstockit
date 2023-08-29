ea707ef3d0c69d65c991956dc9e87e6f819aee59
Bruno Prado <bwprado@gmail.com>
2023-08-29 11:08:17 -0300

    💚 Fixes name import from build

--- src/lib/components/Breadcrumbs.svelte
+++ src/lib/components/Breadcrumbs.svelte
@@ -1,24 +1,25 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-	import { capitalize } from 'lodash';
+    import { page } from "$app/stores"
+    import lodash from "lodash"
+    const { capitalize } = lodash
 
-	$: myBreadcrumbs = $page.url.pathname
-		.split('/')
-		.filter((crumb) => crumb !== '')
-		.map((crumb) => capitalize(crumb));
+    $: myBreadcrumbs = $page.url.pathname
+        .split("/")
+        .filter((crumb) => crumb !== "")
+        .map((crumb) => capitalize(crumb))
 </script>
 
 <div class="h-10 w-full flex items-center justify-start px-4 lg:px-28">
-	<ul class="flex gap-x-4">
-		{#each myBreadcrumbs as crumb, i}
-			{#if i < myBreadcrumbs.length - 1}
-				<li class="crumb"><a class="anchor" href={$page.url.pathname}>{crumb}</a></li>
-				<li class="crumb-separator" aria-hidden>&rsaquo;</li>
-			{:else}
-				<li class="crumb font-bold"><a href="/">Home</a></li>
-				<li class="crumb-separator" aria-hidden>&rsaquo;</li>
-				<li class="crumb">{crumb}</li>
-			{/if}
-		{/each}
-	</ul>
+    <ul class="flex gap-x-4">
+        {#each myBreadcrumbs as crumb, i}
+            {#if i < myBreadcrumbs.length - 1}
+                <li class="crumb"><a class="anchor" href={$page.url.pathname}>{crumb}</a></li>
+                <li class="crumb-separator" aria-hidden>&rsaquo;</li>
+            {:else}
+                <li class="crumb font-bold"><a href="/">Home</a></li>
+                <li class="crumb-separator" aria-hidden>&rsaquo;</li>
+                <li class="crumb">{crumb}</li>
+            {/if}
+        {/each}
+    </ul>
 </div>
