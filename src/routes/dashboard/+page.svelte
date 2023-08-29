754f2ca255e6b806553fb399bb43260453fe0f25
Bruno Prado <bwprado@gmail.com>
2023-08-29 10:02:29 -0300

    💥 Changes to new `Table` component

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,35 +1,18 @@
 <script>
-	export let data;
-	const { dashboard } = data || {};
+    import Table from "$lib/components/Table/Table.svelte"
+
+    export let data
+    const { dashboard } = data || {}
 </script>
 
-<div class="w-1/2 flex flex-col gap-y-2 mx-auto p-8">
-	<table>
-		<thead>
-			<tr class="text-left">
-				<th>Código de barras</th>
-				<th>Produto</th>
-				<th>Quantidade</th>
-				<th>Preço médio</th>
-			</tr>
-		</thead>
-		<tbody>
-			{#if dashboard?.length > 0}
-				{#each dashboard as item}
-					<tr>
-						<td>{item.barcode}</td>
-						<td>{item.product_name}</td>
-						<td>{item.total_amount}</td>
-						<td
-							>{new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(
-								item.average_price
-							)}</td
-						>
-					</tr>
-				{/each}
-			{:else}
-				<span>Nenhum produto encontrado</span>
-			{/if}
-		</tbody>
-	</table>
+<div class="table-container">
+    <Table
+        columns={[
+            { label: "Código", sort: "product_id" },
+            { label: "Produto", sort: "product_name" },
+            { label: "Quantidade", sort: "total_amount" },
+            { label: "Preço Médio", sort: "average_price" },
+        ]}
+        data={dashboard}
+    />
 </div>
