92bb2cb2302be002bcb3f2c4a235608ac7b98794
Bruno Prado <bwprado@gmail.com>
2023-08-29 09:54:55 -0300

    ♻️ Changes to new sidebar instead of header navigation

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,39 +1,27 @@
 <script lang="ts">
-	import { page } from '$app/stores'
-	import { LightSwitch, modeCurrent, setModeCurrent } from '@skeletonlabs/skeleton'
-	export let session: any
-	export let user: any
+    import { page } from "$app/stores"
+    import { Avatar, LightSwitch, modeCurrent, setModeCurrent } from "@skeletonlabs/skeleton"
+    export let session: any
+    export let user: any
 </script>
 
 <header
-	class="flex h-max w-full bg-gray-800 items-center text-gray-200 justify-between py-4 lg:px-28 px-4">
-	<div class="">
-		<a href="/">InventoryVT</a>
-	</div>
-	<nav class="align-middle">
-		<ul class="flex gap-x-6">
-			{#if session && user?.role === 'admin'}
-				<li aria-current={$page.url.pathname === '/admin' ? 'page' : undefined}>
-					<a href="/admin">Admin</a>
-				</li>
-			{/if}
-			<li aria-current={$page.url.pathname === '/dashboard' ? 'page' : undefined}>
-				<a href="/dashboard">Dashboard</a>
-			</li>
-			<li aria-current={$page.url.pathname === '/products' ? 'page' : undefined}>
-				<a href="/products">Products</a>
-			</li>
-			<li aria-current={$page.url.pathname === '/input' ? 'page' : undefined}>
-				<a href="/input">Input</a>
-			</li>
-			<li aria-current={$page.url.pathname.startsWith('/outupt') ? 'page' : undefined}>
-				<a href="/output">Output</a>
-			</li>
-			<li aria-current={$page.url.pathname.startsWith('/login')}>
-				<a href={session ? '/logout' : '/login'}>{session ? 'Logout' : 'Login'}</a>
-			</li>
+    class="flex h-max w-full bg-primary-500 items-center text-gray-200 justify-between py-4 lg:px-28 px-4">
+    <div class="">
+        <a href="/">InventoryVT</a>
+    </div>
+    <nav class="align-middle">
+        <ul class="flex gap-x-6 items-center">
+            <li aria-current={$page.url.pathname.startsWith("/login")}>
+                <a href={session ? "/logout" : "/login"}>{session ? "Logout" : "Login"}</a>
+            </li>
 
-			<LightSwitch on:click={() => setModeCurrent($modeCurrent ? true : false)} />
-		</ul>
-	</nav>
+            <LightSwitch on:click={() => setModeCurrent($modeCurrent ? true : false)} />
+            <Avatar
+                initials={user?.name}
+                border="border-4 border-surface-300-600-token hover:!border-primary-500"
+                width="w-12"
+                cursor="cursor-pointer" />
+        </ul>
+    </nav>
 </header>
