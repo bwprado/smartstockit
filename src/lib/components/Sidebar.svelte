2ed67ea1047a213980aa56b9fdb86a0020fcf043
Bruno Prado <bwprado@gmail.com>
2023-08-29 12:45:04 -0300

    🎨 Adds conditional styling and wraps `a` to `li`

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -5,11 +5,19 @@
     export let navItems: { label: string; link: string }[] = []
     $: active = $page.url.pathname
     const linkStyle = cva(
-        ["py-2", "px-6", "hover:bg-primary-500", "rounded-lg", "flex", "items-center", 'hover:text-primary-50'],
+        [
+            "py-2",
+            "px-6",
+            "hover:bg-primary-500",
+            "rounded-lg",
+            "flex",
+            "items-center",
+            "hover:text-primary-50",
+        ],
         {
             variants: {
                 active: {
-                    true: ["font-bold", 'bg-primary-500'],
+                    true: ["font-bold", "bg-primary-500"],
                     false: "font-medium",
                 },
             },
@@ -20,9 +28,11 @@
 <nav class="h-full w-60 my-auto p-8">
     <ul class="flex flex-col gap-y-4">
         {#each navItems as { link, label }}
-            <li class={linkStyle({ active: active === link })}>
-                <a href={link}>{label}</a>
-            </li>
+            <a href={link}>
+                <li class={linkStyle({ active: active === link })}>
+                    {label}
+                </li>
+            </a>
         {/each}
     </ul>
 </nav>
