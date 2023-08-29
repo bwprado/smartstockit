113ead9e95ef021c686f099f087e92c91289b89e
Bruno Prado <bwprado@gmail.com>
2023-08-29 10:42:39 -0300

    💥 Changes to new `Table` component

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -5,14 +5,22 @@
     const { dashboard } = data || {}
 </script>
 
-<div class="table-container">
-    <Table
-        columns={[
-            { label: "Código", sort: "product_id" },
-            { label: "Produto", sort: "product_name" },
-            { label: "Quantidade", sort: "total_amount" },
-            { label: "Preço Médio", sort: "average_price" },
-        ]}
-        data={dashboard}
-    />
+<div class="flex flex-col gap-y-10 w-full">
+    <section>
+        <h1 class="text-2xl font-bold">Dashboard</h1>
+    </section>
+
+    <div class="table-container">
+        <Table
+            columns={[
+                { label: "Código", sort: "product_id", type: "string" },
+                { label: "Produto", sort: "product_name", type: "string" },
+                { label: "Quantidade", sort: "total_amount", type: "number" },
+                { label: "Preço Médio", sort: "average_price", type: "currency" },
+                { label: "Última Compra", sort: "last_purchase", type: "date" },
+            ]}
+            data={dashboard}
+            index={1}
+        />
+    </div>
 </div>
