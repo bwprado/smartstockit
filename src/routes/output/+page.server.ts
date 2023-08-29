85b0d5a91495cfaea135d307f1de7ce63631d7e8
Bruno Prado <bwprado@gmail.com>
2023-08-29 12:45:39 -0300

    🗃️ Changes data fetched for page

--- src/routes/output/+page.server.ts
+++ src/routes/output/+page.server.ts
@@ -3,11 +3,26 @@ import type { PageServerLoad } from './$types.js';
 
 export interface IProduct { product: string, amount: string, price?: string, fresh?: string, expiration_date?: string }
 
-export const load: PageServerLoad = async ({ locals: { getSession } }) => {
+export const load: PageServerLoad = async ({ locals: { getSession, supabase } }) => {
     if (!await getSession()) {
         throw redirect(303, '/login')
     }
+
+    const { data: outputs, error: err } = await supabase
+        .from('inventory')
+        .select('*, products(name)')
+        .lt('amount', 0)
+        .order('created_at', { ascending: false })
+
+    if (err) {
+        throw err
+    }
+
+    return {
+        outputs: outputs.map(input => ({ ...input, productName: input.products.name }))
+    }
 };
+
 
 export const actions: Actions = {
     default: async ({ request, locals: { supabase, getSession } }) => {
