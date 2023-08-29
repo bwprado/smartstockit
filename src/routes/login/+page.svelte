f4d824fe94e42e9338395cffb1418fa78b265b2c
Bruno Prado <bwprado@gmail.com>
2023-08-29 10:03:57 -0300

    ♻️ Removes `form` prop

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,10 +2,9 @@
 	import { enhance } from '$app/forms';
 	import Button from '$lib/components/Button.svelte';
 	import Input from '$lib/components/Input.svelte';
-	export let form;
 </script>
 
-<form method="post" class="flex flex-col gap-y-4 pt-8">
+<form method="post" class="flex flex-col gap-y-4 pt-8 px-4 sm:max-w-sm mx-auto">
 	<Input id="email" name="email" label="Email" type="text" />
 	<Input id="password" name="password" label="Password" type="password" />
 	<Button type="submit" id="login">Login</Button>
