6809b300ea6bd8d27040a13e5fe01bed35acedb3
Bruno Prado <bwprado@gmail.com>
2023-08-29 09:57:26 -0300

    ♻️  Improved sidebar navigation

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,25 +1,28 @@
 <script lang="ts">
-	import { page } from '$app/stores'
-	import { cva } from 'class-variance-authority'
+    import { page } from "$app/stores"
+    import { cva } from "class-variance-authority"
 
-	export let navItems: { label: string; link: string }[] = []
-	$: active = $page.url.pathname
-	const linkStyle = cva([], {
-		variants: {
-			active: {
-				true: 'font-bold',
-				false: 'font-medium'
-			}
-		}
-	})
+    export let navItems: { label: string; link: string }[] = []
+    $: active = $page.url.pathname
+    const linkStyle = cva(
+        ["py-2", "px-6", "hover:bg-primary-500", "rounded-lg", "flex", "items-center", 'hover:text-primary-50'],
+        {
+            variants: {
+                active: {
+                    true: ["font-bold", 'bg-primary-500'],
+                    false: "font-medium",
+                },
+            },
+        },
+    )
 </script>
 
-<nav class="bg-gray-200 dark:bg-gray-800 dark:text-gray-200 h-full w-36 my-auto p-4">
-	<ul class="flex flex-col gap-y-4">
-		{#each navItems as { link, label }}
-			<li class={linkStyle({ active: active === link })}>
-				<a href={link}>{label}</a>
-			</li>
-		{/each}
-	</ul>
+<nav class="h-full w-60 my-auto p-8">
+    <ul class="flex flex-col gap-y-4">
+        {#each navItems as { link, label }}
+            <li class={linkStyle({ active: active === link })}>
+                <a href={link}>{label}</a>
+            </li>
+        {/each}
+    </ul>
 </nav>
