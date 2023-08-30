12accd85878a5aceaa256e9d481893ac252350f4
Bruno Prado <bwprado@gmail.com>
2023-08-29 22:38:28 -0300

    🚸 Adds notification of sucessful login

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,31 @@
+<script lang="ts">
+    import Button from "$lib/components/Button.svelte"
+    import type { PageData } from "./$types"
+    import { getToastStore } from "@skeletonlabs/skeleton"
+
+    const toastStore = getToastStore()
+
+    export let data: PageData
+    const { user } = data || {}
+
+    if (!user) {
+        toastStore.trigger({
+            message: "Você precisa estar logado para acessar essa página!",
+        })
+    }
+    toastStore.trigger({
+        message: "Login efetuado com sucesso!",
+    })
+</script>
+
 <svelte:head>
     <title>Inventory - VT</title>
 </svelte:head>
 
-<div class="flex items-center justify-center text-gray-900 w-full h-max dark:text-gray-200">
-    <div class="text-center px-4 sm:px-0 lg:px-28">
+<div class="flex items-center justify-center w-full h-max">
+    <div class="text-center px-4 sm:px-0 flex items-center flex-col justify-center">
         <h1 class="text-6xl font-bold mb-6">Inventory - VT</h1>
-        <p class="text-2xl mb-8">
-            A ferramenta definitiva de gerenciamento de estoque para o seu negócio. Simplifique,
-            automatize e tenha controle total do seu estoque como nunca antes.
-        </p>
-        <a
-            href="/login"
-            class="bg-blue-600 text-white px-8 py-3 rounded-md hover:bg-blue-500 transition duration-300"
-            >Comece Agora</a
-        >
+        <p class="text-2xl mb-8">Seja bem vindo {user?.name}</p>
+        <Button href="/dashboard">Comece Agora</Button>
     </div>
 </div>
