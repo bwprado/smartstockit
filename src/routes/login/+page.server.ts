e2fd14d64e1d51d4deec54d0e7b41f9500830719
Bruno Prado <bwprado@gmail.com>
2023-08-29 22:39:56 -0300

    🎨 Adds space

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,12 +1,12 @@
 import { AuthApiError } from "@supabase/supabase-js"
 import { redirect, type Actions, error } from "@sveltejs/kit"
+import delay from 'lodash/delay'
 
 export const actions: Actions = {
     default: async ({ request, locals: { supabase } }) => {
         const loginData = Object.fromEntries(await request.formData()) || {}
 
         const { error: err } = await supabase.auth.signInWithPassword(loginData as { email: string, password: string })
-
         if (err) {
             if (err instanceof AuthApiError && err.status === 400) {
                 return error(401, err.message)
