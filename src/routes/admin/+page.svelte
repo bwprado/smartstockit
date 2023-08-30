e2c0f67140067475fb1ef11a02a0e2a94df184a7
Bruno Prado <bwprado@gmail.com>
2023-08-29 22:39:11 -0300

    💥 Refactors to new `Table` component

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,49 +1,29 @@
 <script lang="ts">
-	import type { PageData } from './$types';
+    import Button from "$lib/components/Button.svelte"
+    import Table from "$lib/components/Table/Table.svelte"
+    import type { PageData } from "./$types"
 
-	export let data: PageData;
+    export let data: PageData
 
-	const users = data?.users ?? [];
+    const users = data?.users ?? []
 </script>
 
 <svelte:head>
-	<title>Inventory - VT / Admin</title>
+    <title>Inventory - VT / Admin</title>
 </svelte:head>
-<div class="w-fit flex flex-col gap-y-2 mx-auto p-8">
-	<table class="table-auto">
-		<thead>
-			<tr class="text-left">
-				<th class="p-2">#</th>
-				<th class="p-2">Nome</th>
-				<th class="p-2">Email</th>
-				<th class="p-2">Tipo</th>
-				<th class="p-2" />
-				<th class="p-2" />
-			</tr>
-		</thead>
-		<tbody>
-			{#if users?.length > 0}
-				{#each users as user, index}
-					<tr>
-						<td class="p-2">{index}</td>
-						<td class="w-max whitespace-nowrap">{user?.name}</td>
-						<td class="p-2">{user?.email}</td>
-						<td class="p-2">{user?.role}</td>
-						<td class="p-2">
-							<a href="/admin/users/edit/{user?.id}">
-								<i class="mi mi-edit"><span class="u-sr-only" />Editar </i>
-							</a>
-						</td>
-						<td class="p-2">
-							<a href="/admin/users/remove/{user?.id}">
-								<i class="mi mi-delete"><span class="u-sr-only">Remover</span></i>
-							</a>
-						</td>
-					</tr>
-				{/each}
-			{:else}
-				<tr>Nenhum usuário encontrado</tr>
-			{/if}
-		</tbody>
-	</table>
+
+<div class="flex justify-between items-center py-4">
+    <h1 class="text-xl font-bold">Usuários</h1>
+    <Button>Adicionar Usuário</Button>
+</div>
+
+<div class="table-container">
+    <Table
+        columns={[
+            { label: "Nome", key: "name", type: "string" },
+            { label: "Email", key: "email", type: "string" },
+            { label: "Tipo", key: "role", type: "string" },
+        ]}
+        data={users}
+    />
 </div>
