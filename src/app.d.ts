94ea6769c89e12098c154e619763e35078c1cb19
Bruno Prado <bwprado@gmail.com>
2023-09-04 12:18:43 -0300

    🏷️ Updates types for app

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,27 +1,28 @@
 import { SupabaseClient, Session, User } from '@supabase/supabase-js'
+import type { IProfile } from './hooks.server'
 
 declare global {
     namespace App {
         interface Locals {
             supabase: SupabaseClient
             getSession(): Promise<Session | null>
-            getUserProfile(): Promise<{ id: string, name: string, email: string, role: "user" | "admin" }>
+            getUserProfile(): Promise<IProfile>
             signIn(email: string, password: string): Promise<{ user: User, session: Session }>
             signOut(): void
         }
         interface PageData {
             session: Promise<Session | null>
-            user: Promise<{ id: string, name: string, email: string, role: "user" | "admin" }>
+            user: Promise<IProfile>
         }
 
         interface LayoutData {
             session: Promise<Session | null>
-            user: Promise<{ id: string, name: string, email: string, role: "user" | "admin" }>
+            user: Promise<IProfile>
         }
 
         interface LayoutServerLoad {
             session: Promise<Session | null>
-            user: Promise<{ id: string, name: string, email: string, role: "user" | "admin" }>
+            user: Promise<IProfile>
         }
     }
 }
