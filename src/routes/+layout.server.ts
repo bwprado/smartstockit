8f2f656a149f8ee5837bd7b41a260fc5c429a5b5
Bruno Prado <bwprado@gmail.com>
2023-09-04 12:22:36 -0300

    🧑‍💻 Adds types

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,18 +1,33 @@
 import type { LayoutServerLoad } from "./$types"
 
+export interface IProduct {
+    name: string
+    id: string
+    balance: number
+    unit: string
+    created_at: string
+}
+export interface IDashboard {
+    product_name: string
+    total_amount: number
+    average_price: number
+    last_entry: string
+}
+
 export const load: LayoutServerLoad = async ({ locals: { supabase, getSession, getUserProfile } }) => {
     const session = await getSession()
 
-    const fetchProductsData = async () => {
+
+    const fetchProductsData = async (): Promise<IProduct[]> => {
         if (!session) return []
 
         const { data, error } = await supabase
             .from('products')
             .select()
-
         return session ? error ? [] : data : []
     }
-    const fetchDashboardData = async () => {
+    const fetchDashboardData = async (): Promise<IDashboard[]> => {
+
         if (!session) return []
 
         const { data, error } = await supabase.rpc('get_product_summary')
