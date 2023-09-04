394f5ce053fec1ca1939efc26ae650e8db9fc4c9
Bruno Prado <bwprado@gmail.com>
2023-09-04 12:19:02 -0300

    Type for profiles

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,6 +2,14 @@ import { PUBLIC_SUPABASE_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public'
 import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit'
 import { error, type Handle } from '@sveltejs/kit'
 
+export interface IProfile {
+    id: string
+    role: "user" | "admin"
+    name: string
+    email: string
+
+}
+
 export const handle: Handle = async ({ event, resolve }) => {
     event.locals.supabase = createSupabaseServerClient({
         supabaseUrl: PUBLIC_SUPABASE_URL as string,
@@ -25,7 +33,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 
     event.locals.getUserProfile = async () => {
         const { data: { session } } = await event.locals.supabase.auth.getSession()
-        
+
         if (!session) return null
 
         const { data, error: err } = await event.locals.supabase.from('profiles').select('*').eq('id', session.user.id).single()
