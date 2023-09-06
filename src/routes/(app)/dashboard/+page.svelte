428abc01622bee66c0c15627dd2ee33646f1bcac
Bruno Prado <bwprado@gmail.com>
2023-09-06 16:10:01 -0300

    🚚 Moves to new folder

---
+++ src/routes/(app)/dashboard/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    import Table from "$lib/components/Table/Table.svelte"
+
+    export let data
+    const { dashboard } = data || {}
+</script>
+
+<div class="flex flex-col gap-y-10 w-full">
+    <section>
+        <h1 class="text-2xl font-bold">Dashboard</h1>
+    </section>
+
+    <div class="table-container">
+        <Table
+            columns={[
+                { label: "Produto", key: "product_name", type: "string" },
+                { label: "Quantidade", key: "total_amount", type: "number" },
+                { label: "Preço Médio", key: "average_price", type: "currency" },
+                { label: "Último Registro", key: "last_entry", type: "date" },
+            ]}
+            data={dashboard}
+            index={1}
+        />
+    </div>
+</div>
