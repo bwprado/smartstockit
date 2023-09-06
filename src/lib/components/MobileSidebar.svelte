51f5568ad5ff49764ad445233f8b9fb0840e6971
Bruno Prado <bwprado@gmail.com>
2023-09-06 16:07:47 -0300

    ✨ New sidebar exclusive for mobile devices

---
+++ src/lib/components/MobileSidebar.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+    import { page } from "$app/stores"
+    import { cva } from "class-variance-authority"
+    import IconButton from "./IconButton.svelte"
+    import { X } from "lucide-svelte"
+
+    export let navItems: { label: string; link: string }[] = []
+    export let onClick: () => void = () => {}
+
+    $: active = $page.url.pathname
+    const linkStyle = cva(
+        [
+            "py-2",
+            "px-6",
+            "hover:bg-primary-500",
+            "rounded-lg",
+            "flex",
+            "items-center",
+            "hover:text-primary-50",
+        ],
+        {
+            variants: {
+                active: {
+                    true: ["font-bold", "bg-primary-500", "text-white"],
+                    false: "font-medium",
+                },
+            },
+        },
+    )
+</script>
+
+<nav
+    class="bg-gray-200 dark:bg-gray-800 h-full flex flex-col gap-y-20 w-full transition-all duration-300 p-8 rounded-lg"
+>
+    <div class="flex items-center justify-between">
+        <h1 class="text-primary-500 text-2xl font-bold">Inventory - VT</h1>
+        <IconButton on:click={onClick}>
+            <X />
+        </IconButton>
+    </div>
+    <ul class="flex-col gap-y-4 flex">
+        {#each navItems as { link, label }}
+            <a href={link} on:click={onClick}>
+                <li class={linkStyle({ active: active === link })}>
+                    {label}
+                </li>
+            </a>
+        {/each}
+    </ul>
+    <footer class="flex justify-center items-center mt-auto">
+        <p class="text-xs py-4">
+            <a class="font-bold" href="mailto:bwprado@gmail.com">Working In Progress Dev </a> - All rights
+            reserved 2023.
+        </p>
+    </footer>
+</nav>
