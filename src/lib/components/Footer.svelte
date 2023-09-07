f868a2d689b07fbfdf6b36e18ba54cd140dc7bc4
Bruno Prado <bwprado@gmail.com>
2023-09-07 20:59:33 -0300

    ✨ `Footer` component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { twMerge } from "tailwind-merge"
+
+    export let customClass = ""
+</script>
+
+<footer class={twMerge("text-surface-400 text-center text-xs flex flex-col gap-y-3", customClass)}>
+    <p>&copy; 2023 <strong>Smart Stock It</strong>.</p>
+    <p class="text-[10px]">
+        Todos os direitos reservados para <strong>Working in Progress Dev</strong>.
+    </p>
+</footer>
