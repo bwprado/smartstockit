b0787f16f9df1ba3b334c0f0b7c793d7912cf823
Bruno Prado <bwprado@gmail.com>
2023-09-07 20:58:52 -0300

    💄 Decrease font size

--- src/lib/components/Breadcrumbs.svelte
+++ src/lib/components/Breadcrumbs.svelte
@@ -9,7 +9,7 @@
         .map((crumb) => capitalize(crumb))
 </script>
 
-<div class="h-10 w-full flex items-center justify-start px-4 lg:px-28">
+<div class="h-10 w-full flex items-center justify-start px-4 lg:px-28 text-xs">
     <ul class="flex gap-x-4">
         {#each myBreadcrumbs as crumb, i}
             {#if i < myBreadcrumbs.length - 1}
