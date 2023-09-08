6f09527b7509497059901a83a1cbe6a01d0786c9
Bruno Prado <bwprado@gmail.com>
2023-09-07 21:00:07 -0300

    ♻️ Adds `anchor` to button

--- src/lib/components/IconButton.svelte
+++ src/lib/components/IconButton.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
-    import { cva, cx } from "class-variance-authority"
+    import { cx } from "class-variance-authority"
     import { twMerge } from "tailwind-merge"
 
     export let id: string = "button"
     export let type: "button" | "submit" | "reset" = "button"
     export let customClasses: string = ""
     export let disabled: boolean = false
+    export let href: string = ""
+
     const btn = cx([
         "rounded-full",
         "hover:opacity-80",
@@ -21,6 +23,12 @@
     ])
 </script>
 
-<button class={twMerge(btn, customClasses)} {id} {type} {disabled} on:click>
-    <slot />
-</button>
+{#if href}
+    <a class={twMerge(btn, customClasses)} {href} {id} {type} {disabled} on:click>
+        <slot />
+    </a>
+{:else}
+    <button class={twMerge(btn, customClasses)} {id} {type} {disabled} on:click>
+        <slot />
+    </button>
+{/if}
