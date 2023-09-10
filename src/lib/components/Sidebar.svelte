51411a8ff04f54d4eb0d1f3a72e2c60d4cbb5d4d
Bruno Prado <bwprado@gmail.com>
2023-09-10 04:16:44 -0300

    💥 Major migration from props to slots

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,42 +1,9 @@
 <script lang="ts">
-    import { page } from "$app/stores"
-    import { cva } from "class-variance-authority"
-    import type { SvelteComponent } from "svelte"
-
-    export let navItems: { label: string; link: string; Icon: SvelteComponent }[] = []
-    $: active = $page.url.pathname
-    const linkStyle = cva(
-        [
-            "py-2",
-            "px-6",
-            "hover:bg-primary-500",
-            "rounded-lg",
-            "flex",
-            "items-center",
-            "gap-x-4",
-            "hover:text-primary-50",
-        ],
-        {
-            variants: {
-                active: {
-                    true: ["font-bold", "bg-primary-500"],
-                    false: "font-medium",
-                },
-            },
-        },
-    )
 </script>
 
 <nav
-    class="h-full w-0 sm:w-60 transition-all duration-300 my-auto p-2 sm:p-8 hidden lg:block bg-surface-800">
+    class="h-full w-0 sm:w-fit transition-all duration-300 my-auto p-2 sm:p-8 hidden lg:block bg-surface-800">
     <ul class="flex-col gap-y-4 flex">
-        {#each navItems as { link, label, Icon }}
-            <a href={link}>
-                <li class={linkStyle({ active: active === link })}>
-                    <svelte:component this={Icon} />
-                    <span>{label}</span>
-                </li>
-            </a>
-        {/each}
+        <slot />
     </ul>
 </nav>
--- src/lib/components/SidebarItem.svelte
+++ src/lib/components/SidebarItem.svelte
@@ -1,3 +1,38 @@
 <script lang="ts">
+    import { page } from "$app/stores"
+    import { cva } from "class-variance-authority"
+
     export let label: string = ""
+    export let link: string = ""
+    export let active: boolean = false
+
+    const linkStyle = cva(
+        [
+            "py-2",
+            "px-4",
+            "hover:bg-primary-500",
+            "rounded-lg",
+            "flex",
+            "items-center",
+            "gap-x-4",
+            "hover:text-primary-50",
+        ],
+        {
+            variants: {
+                active: {
+                    true: ["font-bold", "bg-primary-500"],
+                    false: "font-medium",
+                },
+            },
+        },
+    )
 </script>
+
+<a href={link} class="w-full">
+    <li class={linkStyle({ active: $page.url.pathname === link })}>
+        <div class="w-fit">
+            <slot />
+        </div>
+        <span>{label}</span>
+    </li>
+</a>
