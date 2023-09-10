4fde55b3988cc0dd7d3ef61a22b361c561f23f84
Bruno Prado <bwprado@gmail.com>
2023-09-10 04:17:45 -0300

    ♻️ Updates `Sidebar` component to new slotted one

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -6,17 +6,19 @@
     import type { LayoutData } from "./$types"
 
     import Breadcrumbs from "$lib/components/Breadcrumbs.svelte"
+    import Footer from "$lib/components/Footer.svelte"
     import Header from "$lib/components/Header.svelte"
     import Sidebar from "$lib/components/Sidebar.svelte"
-    import { ArrowDown, ArrowUp, LayoutDashboard, Package, Settings } from "lucide-svelte"
-    import Footer from "$lib/components/Footer.svelte"
+    import SidebarItem from "$lib/components/SidebarItem.svelte"
+    import { ArrowDown, ArrowUp, Box, LayoutDashboard, Settings, Settings2 } from "lucide-svelte"
 
     storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow })
 
     initializeStores()
 
     export let data: LayoutData
-    const { user } = data || {}
+
+    const { session, profile } = data
 </script>
 
 <Toast />
@@ -25,28 +27,37 @@
 <AppShell>
     <svelte:fragment slot="header">
         <title>Smart Stock It{` - ${$page.url.pathname}`}</title>
-        <Header session={data?.session || false} user={data?.user} />
+        {#if profile && session}
+            <Header {session} {profile} />
+        {/if}
     </svelte:fragment>
-    {#if user}
-        <Breadcrumbs />
-    {/if}
     <svelte:fragment slot="sidebarLeft">
-        {#if user}
-            <Sidebar
-                navItems={[
-                    { label: "Configurações", link: "/settings", Icon: Settings },
-                    { label: "Dashboard", link: "/dashboard", Icon: LayoutDashboard },
-                    { label: "Produtos", link: "/products", Icon: Package },
-                    { label: "Entradas", link: "/input", Icon: ArrowUp },
-                    { label: "Saídas", link: "/output", Icon: ArrowDown },
-                ]} />
+        {#if profile}
+            <Sidebar>
+                <SidebarItem label="Configurações" link="/settings">
+                    <Settings size={20} />
+                </SidebarItem>
+                <SidebarItem label="Dashboard" link="/dashboard">
+                    <LayoutDashboard size={20} />
+                </SidebarItem>
+                <SidebarItem label="Produtos" link="/products">
+                    <Box size={20} />
+                </SidebarItem>
+                <SidebarItem label="Entradas" link="/input">
+                    <ArrowUp size={20} />
+                </SidebarItem>
+                <SidebarItem label="Saídas" link="/output">
+                    <ArrowDown size={20} />
+                </SidebarItem>
+            </Sidebar>
         {/if}
     </svelte:fragment>
 
-    <div class="px-4 lg:px-8 xl:px-20 h-full">
+    <div class="px-4 lg:px-8 xl:px-20 h-full flex flex-col gap-y-10">
+        <Breadcrumbs />
         <slot />
     </div>
     <svelte:fragment slot="pageFooter">
-        <Footer customClass="pb-4" />
+        <Footer class="pb-4" />
     </svelte:fragment>
 </AppShell>
