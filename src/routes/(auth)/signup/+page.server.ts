eb942670e3bfd6c0d83e496f09594aa8563db83e
Bruno Prado <bwprado@gmail.com>
2023-09-10 04:22:26 -0300

    ✨ Adds `Google` login option to form

--- src/routes/(auth)/signup/+page.server.ts
+++ src/routes/(auth)/signup/+page.server.ts
@@ -2,11 +2,14 @@ import { redirect } from "@sveltejs/kit"
 import type { Actions } from "./$types"
 
 export const actions: Actions = {
-    signup: async ({ request, locals: { supabase } }) => {
+    email: async ({ request, locals: { supabase } }) => {
         const data = await request.formData()
         const email = data.get("email") as string
 
-        const { error } = await supabase.auth.signInWithOtp({ email })
+        const { error } = await supabase.auth.signInWithOtp({
+            email,
+            options: { emailRedirectTo: "https://smartstockit.com/" },
+        })
 
         if (error) {
             return {
@@ -14,6 +17,20 @@ export const actions: Actions = {
                 body: error.message,
             }
         }
-        throw redirect(303, "signup/verify")
+        throw redirect(303, `signup/verify?email=${email}`)
+    },
+    google: async ({ locals: { supabase } }) => {
+        const { data, error: err } = await supabase.auth.signInWithOAuth({
+            provider: "google",
+        })
+
+        if (err) {
+            return {
+                status: 400,
+                body: err.message,
+            }
+        }
+
+        throw redirect(303, "/")
     },
 }
