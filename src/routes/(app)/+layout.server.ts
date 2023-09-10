f0e19fca63f8fabe65b8c69730e4dd27c5f06009
Bruno Prado <bwprado@gmail.com>
2023-09-10 04:17:23 -0300

    💥 Major change to server load functions

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -1,45 +1,8 @@
 import type { LayoutServerLoad } from "./$types"
 
-export interface IProduct {
-    name: string
-    id: string
-    balance: number
-    unit: string
-    created_at: string
-}
-export interface IDashboard {
-    product_name: string
-    total_amount: number
-    average_price: number
-    last_entry: string
-}
-
-export const load: LayoutServerLoad = async ({ locals: { supabase, getSession, getUserProfile } }) => {
-    const session = await getSession()
-
-
-    const fetchProductsData = async (): Promise<IProduct[]> => {
-        if (!session) return []
-
-        const { data, error } = await supabase
-            .from('products')
-            .select()
-            .eq('user', session.user.id)
-            
-        return session ? error ? [] : data : []
-    }
-    const fetchDashboardData = async (): Promise<IDashboard[]> => {
-
-        if (!session) return []
-
-        const { data, error } = await supabase.rpc('get_product_summary')
-
-        return error ? [] : data
-    }
+export const load: LayoutServerLoad = async ({ locals: { getSession, getProfile } }) => {
     return {
-        products: fetchProductsData(),
-        dashboard: fetchDashboardData(),
         session: getSession(),
-        user: getUserProfile()
+        profile: getProfile(),
     }
 }
