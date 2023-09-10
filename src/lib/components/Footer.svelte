c7ef3b293c4dfcac4f7e4c41f66ba3f2f63f122e
Bruno Prado <bwprado@gmail.com>
2023-09-10 04:12:35 -0300

    ♻️ Changes from `className` to `class` prop

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,10 +1,12 @@
-<script>
+<script lang="ts">
     import { twMerge } from "tailwind-merge"
 
-    export let customClass = ""
+    let className: string = ""
+
+    export { className as class }
 </script>
 
-<footer class={twMerge("text-surface-400 text-center text-xs flex flex-col gap-y-3", customClass)}>
+<footer class={twMerge("text-surface-400 text-center text-xs flex flex-col gap-y-3", className)}>
     <p>&copy; 2023 <strong>Smart Stock It</strong>.</p>
     <p class="text-[10px]">
         Todos os direitos reservados para <strong>Working in Progress Dev</strong>.
