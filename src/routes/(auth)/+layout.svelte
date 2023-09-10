634ab342517657fc9375636aa70f97474cb0bfd1
Bruno Prado <bwprado@gmail.com>
2023-09-10 04:20:22 -0300

    ✨ Adds slots to component

--- src/routes/(auth)/+layout.svelte
+++ src/routes/(auth)/+layout.svelte
@@ -1,15 +1,20 @@
 <script>
     import { Toast, initializeStores } from "@skeletonlabs/skeleton"
+    import { page } from "$app/stores"
     import "../../app.css"
+    import Footer from "$lib/components/Footer.svelte"
 
     initializeStores()
 </script>
 
 <Toast />
-
-<div class="h-auto w-auto">
+<svelte:head>
+    <title>Smart Stock It - {$page.url.pathname === "/login" ? "Login" : "Criar Conta"}</title>
+</svelte:head>
+<div class="h-auto lg:h-[calc(100vh-44px)] w-auto flex items-center justify-center">
     <slot />
 </div>
+<Footer />
 
 <style>
     :root {
