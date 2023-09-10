2c7bf16a2b70f565bd404dc7423a632b715ef0e5
Bruno Prado <bwprado@gmail.com>
2023-09-10 04:11:06 -0300

    ✨ New chart component

---
+++ src/lib/components/Chart.svelte
@@ -0,0 +1,36 @@
+<script>
+    import Chart from "chart.js/auto"
+    import { onDestroy, onMount } from "svelte"
+
+    let ctx
+    let chart
+
+    onMount(() => {
+        chart = new Chart(ctx, {
+            type: "bar",
+            data: {
+                labels: ["Bolo de 1Kg", "Açucar Mascavo", "Trigo", "Cookies", "Purple", "Orange"],
+                datasets: [
+                    {
+                        label: "Entrada de produtos",
+                        data: [12, 19, 3, 5, 2, 3],
+                        borderWidth: 1,
+                        borderRadius: 8,
+                    },
+                ],
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                    },
+                },
+            },
+        })
+    })
+    onDestroy(() => {
+        chart.destroy()
+    })
+</script>
+
+<canvas id="myChart" width="400" height="400" bind:this={ctx}></canvas>
