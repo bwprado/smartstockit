874ded8074ec318e36fde7c12f97c936cb574589
Bruno Prado <bwprado@gmail.com>
2023-09-10 04:10:26 -0300

    💄 Changes in style

--- src/lib/components/Breadcrumbs.svelte
+++ src/lib/components/Breadcrumbs.svelte
@@ -9,16 +9,16 @@
         .map((crumb) => capitalize(crumb))
 </script>
 
-<div class="h-10 w-full flex items-center justify-start px-4 lg:px-28 text-xs">
+<div class="h-10 w-full flex items-center justify-start text-xs">
     <ul class="flex gap-x-4">
         {#each myBreadcrumbs as crumb, i}
             {#if i < myBreadcrumbs.length - 1}
                 <li class="crumb"><a class="anchor" href={$page.url.pathname}>{crumb}</a></li>
                 <li class="crumb-separator" aria-hidden>&rsaquo;</li>
             {:else}
-                <li class="crumb font-bold"><a href="/">Home</a></li>
+                <li class="crumb"><a href="/dashboard">Home</a></li>
                 <li class="crumb-separator" aria-hidden>&rsaquo;</li>
-                <li class="crumb">{crumb}</li>
+                <li class="crumb font-bold">{crumb}</li>
             {/if}
         {/each}
     </ul>
