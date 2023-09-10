250538831ec5137de63bb8bba535436c50fe01de
Bruno Prado <bwprado@gmail.com>
2023-09-10 04:14:59 -0300

    ✨ New `PageHeader` to normalize design

---
+++ src/lib/components/PageHeader.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { twMerge } from "tailwind-merge"
+
+    let className: string = ""
+    export { className as class }
+    export let title: string = ""
+</script>
+
+<section class={twMerge("flex flex-col sm:flex-row justify-between gap-4", className)}>
+    <h1 class="text-xl sm:text-2xl font-bold whitespace-nowrap">{title}</h1>
+    <slot title="action" />
+</section>
