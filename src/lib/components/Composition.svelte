56835d4b90631e12a18c8ddc94d49b0584895d8d
Bruno Prado <bwprado@gmail.com>
2023-09-10 04:11:50 -0300

    ✨ New `Compostion` component to get `raw` products into products

---
+++ src/lib/components/Composition.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import { twMerge } from "tailwind-merge"
+
+    export let label: string = ""
+    export let name: string = "input"
+    export let id: string = "input"
+    export let customClasses: {
+        wrapper?: string
+        label?: string
+        input?: string
+    } = {}
+</script>
+
+{#if label}
+    <label
+        for={name}
+        class={twMerge(
+            "block text-sm leading-6 text-gray-900 font-bold dark:text-gray-200",
+            customClasses.label,
+        )}>{label}</label>
+{/if}
