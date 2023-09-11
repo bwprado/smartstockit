839b610c7502db11ec4244bd535ad8d6765d77a2
Bruno Prado <bwprado@gmail.com>
2023-09-11 00:18:37 -0300

    🚧 Component still in development, not working properly

--- src/lib/components/Composition.svelte
+++ src/lib/components/Composition.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
     import { twMerge } from "tailwind-merge"
+    import Button from "./Button.svelte"
+    import CompositionItem from "./CompositionItem.svelte"
 
     export let label: string = ""
     export let name: string = "input"
-    export let id: string = "input"
     export let customClasses: {
         wrapper?: string
         label?: string
         input?: string
     } = {}
+    export let options: { value: string; label: string }[] = []
+    let composition: { value: string; label: string; amount: number }[] = []
 </script>
 
 {#if label}
@@ -19,3 +22,35 @@
             customClasses.label,
         )}>{label}</label>
 {/if}
+{#if composition.length === 0}
+    <p class="text-xs text-surface-900 dark:text-surface-200">Nenhum insumo adicionado</p>
+{:else}
+    <table id="raw_material" class="rounded-t-md">
+        <thead class="rounded-t-md">
+            <tr class="text-xs text-surface-900 dark:text-surface-200 bg-surface-700 leading-6">
+                <th class="px-3 rounded-s-md">Insumo</th>
+                <th class="px-3 rounded-e-md">Quantidade</th>
+            </tr>
+        </thead>
+        <tbody class="relative">
+            {#each composition as item}
+                {#key item.value}
+                    <CompositionItem
+                        key={item.value}
+                        {options}
+                        bind:compItem={item}
+                        on:click={() => {
+                            composition = composition.filter((c) => c != item)
+                        }} />
+                {/key}
+            {/each}
+        </tbody>
+    </table>
+    <div class="text-white">{JSON.stringify(composition)}</div>
+{/if}
+<Button
+    intent="secondary"
+    class="w-fit text-xs"
+    on:click={() => (composition = [...composition, { value: "", label: "", amount: 0 }])}>
+    Adicionar Insumo
+</Button>
