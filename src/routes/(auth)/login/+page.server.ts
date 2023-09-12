ac56f8f4ce7056f018cae92e0b48a4db2fc7500c
Bruno Prado <bwprado@gmail.com>
2023-09-12 01:45:25 -0300

    ♻️ Changes `google` login to use params for `provider`

--- src/routes/(auth)/login/+page.server.ts
+++ src/routes/(auth)/login/+page.server.ts
@@ -1,19 +1,35 @@
-import { AuthApiError } from "@supabase/supabase-js"
-import { redirect } from "@sveltejs/kit"
+import { AuthApiError, type Provider } from "@supabase/supabase-js"
+import { fail, redirect } from "@sveltejs/kit"
 import type { Actions } from "./$types"
 
 export const actions: Actions = {
-    "email-password": async ({ request, locals: { supabase, getSession } }) => {
+    default: async ({ request, locals: { supabase, getSession }, url }) => {
+        const provider = url.searchParams.get("provider") as Provider
+
+        if (provider === "google") {
+            const { data, error } = await supabase.auth.signInWithOAuth({
+                provider,
+            })
+
+            if (error instanceof AuthApiError) {
+                console.log(error)
+                return fail(error.status, { message: error.message })
+            }
+            console.log(data)
+            throw redirect(301, data?.url as string)
+        }
+
         const loginData = Object.fromEntries(await request.formData()) || {}
 
         const { data, error: err } = await supabase.auth.signInWithPassword(
             loginData as { email: string; password: string },
         )
+
         if (err) {
             if (err instanceof AuthApiError && err.status === 400) {
-                return { status: 400, body: "Email ou password inválidos." }
+                return fail(400, { message: "Email ou password inválidos." })
             } else {
-                return { status: 500, body: "Erro desconhecido." }
+                return fail(500, { message: "Erro desconhecido." })
             }
         }
 
@@ -28,23 +44,10 @@ export const actions: Actions = {
 
             if (userErr) {
                 console.log(userErr)
-                return { status: 500, body: "Erro desconhecido." }
+                return fail(500, { message: "Erro desconhecido." })
             }
         }
 
         throw redirect(301, "/dashboard")
-    },
-    google: async ({ locals: { supabase } }) => {
-        // const { data, error } = await supabase.auth.signInWithIdToken({
-        //     token: response.credential,
-        //     nonce: "NONCE", // must be the same one as provided in data-nonce (if any)
-        // })
-        // if (err) {
-        //     console.log(err)
-        //     return { status: 500, body: "Erro desconhecido." }
-        // }
-        // console.log("Login successful")
-        // console.log(data)
-        // throw redirect(301, "/dashboard")
     },
 }
