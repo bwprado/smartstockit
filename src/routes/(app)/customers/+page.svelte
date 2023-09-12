d43373bd98aa0007127056aed2de56fb33271380
Bruno Prado <bwprado@gmail.com>
2023-09-12 01:44:27 -0300

    🚧 New page for testing composition component now

---
+++ src/routes/(app)/customers/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import CompositionItem from "$lib/components/CompositionItem.svelte"
+    import Composition from "$lib/components/Composition.svelte"
+</script>
+
+<Composition options={[{ value: "sugar", label: "Açucar" }]}>
+    <CompositionItem />
+</Composition>
