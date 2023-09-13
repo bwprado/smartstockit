bc73ba5c4a4b58d1587785d40b66ba30776e9e05
Bruno Prado <bwprado@gmail.com>
2023-09-13 10:52:28 -0300

    💥 Major changes to binded values

--- src/lib/components/CompositionItem.svelte
+++ src/lib/components/CompositionItem.svelte
@@ -1,39 +1,40 @@
 <script lang="ts">
+    import type { AutocompleteOption } from "@skeletonlabs/skeleton"
+    import { uniqueId } from "lodash"
     import { Minus } from "lucide-svelte"
+    import { createEventDispatcher } from "svelte"
     import IconButton from "./IconButton.svelte"
     import Input from "./Input.svelte"
     import SelectSearch from "./SelectSearch.svelte"
-    import type { Autocomplete, AutocompleteOption } from "@skeletonlabs/skeleton"
-    import { createEventDispatcher } from "svelte"
 
     export let key: string = ""
-    export let compItem: { value: string; label: string; amount: number } = {
-        value: "",
-        label: "",
-        amount: 0,
-    }
+    export let value: string = ""
+    export let label: string = ""
+    export let amount: number = 0
     export let options: { value: string; label: string }[] = []
 
     const handleInput = (e: Event) => {
         const input = e?.currentTarget as HTMLInputElement
-        compItem.amount = +input.value
+        amount = +input.value
     }
 
     const dispatch = createEventDispatcher()
     const handleSelection = (e: CustomEvent<AutocompleteOption>) => {
-        compItem.value = e.detail.value as string
-        compItem.label = e.detail.label
+        value = e.detail.value as string
+        label = e.detail.label
         dispatch("selection", e.detail)
     }
+    const idSearch = uniqueId()
+    const idAmount = uniqueId()
 </script>
 
 <tr class="relative" id={key}>
     <td class="first:w-4/5">
         <SelectSearch
             {options}
-            name={compItem.label}
-            id={compItem.value}
-            bind:inputValue={compItem.label}
+            name={idSearch}
+            id={idSearch}
+            bind:inputValue={label}
             on:selection={handleSelection}
             placeholder="Digite o nome do insumo"
             customClasses={{
@@ -46,8 +47,9 @@
         <Input
             placeholder="0"
             type="number"
-            name={`${compItem.value}-amount`}
-            id={`${compItem.value}-amount`}
+            value={`${amount}`}
+            name={idAmount}
+            id={idAmount}
             customClasses={{
                 wrapper: "w-full",
                 input: "w-full rounded-e-md rounded-s-none",
@@ -55,6 +57,9 @@
             on:input={handleInput} />
     </td>
     <td class="pl-2 right-0">
-        <IconButton intent="secondary" on:click><Minus size={15} /></IconButton>
+        <IconButton intent="secondary" on:click
+            ><svelte:fragment slot="icon">
+                <Minus size={15} />
+            </svelte:fragment></IconButton>
     </td>
 </tr>
