bf8403682408ab994ae3af8316cb0866783f8613
Bruno Prado <bwprado@gmail.com>
2023-09-22 01:35:26 -0300

    ✨ New `SidebarSubItem` component

---
+++ src/lib/components/SidebarSubItem.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { page } from "$app/stores"
+    import { CornerDownRight } from "lucide-svelte"
+
+    export let label: string
+    export let link: string
+    $: active = $page.url.pathname === link
+</script>
+
+<li>
+    <a
+        href={link}
+        data-active={active}
+        class="flex px-6 py-2 gap-x-4 text-surface-300 rounded-lg hover:text-secondary-500 data-[active=true]:text-secondary-500 data-[active=true]:font-bold">
+        <CornerDownRight size={20} />
+        <span>{label}</span>
+    </a>
+</li>
