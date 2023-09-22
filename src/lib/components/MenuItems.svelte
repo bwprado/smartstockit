92844cc2a18c88f866633343140f240fd3c4668c
Bruno Prado <bwprado@gmail.com>
2023-09-22 01:33:54 -0300

    ✨ Adds new component for sidebar and new link

--- src/lib/components/MenuItems.svelte
+++ src/lib/components/MenuItems.svelte
@@ -2,23 +2,25 @@
     import { ArrowDown, ArrowUp, Box, LayoutDashboard, Settings, UserSquare2 } from "lucide-svelte"
 
     import SidebarItem from "./SidebarItem.svelte"
+    import SidebarSubItem from "./SidebarSubItem.svelte"
 </script>
 
 <SidebarItem label="Dashboard" link="/dashboard">
-    <LayoutDashboard size={20} />
+    <LayoutDashboard size={20} slot="icon" />
 </SidebarItem>
 <SidebarItem label="Produtos" link="/products">
-    <Box size={20} />
+    <Box size={20} slot="icon" />
+    <SidebarSubItem label="Categorias" link="/categories" slot="subitem" />
 </SidebarItem>
 <SidebarItem label="Clientes" link="/customers">
-    <UserSquare2 size={20} />
+    <UserSquare2 size={20} slot="icon" />
 </SidebarItem>
 <SidebarItem label="Entradas" link="/input">
-    <ArrowUp size={20} />
+    <ArrowUp size={20} slot="icon" />
 </SidebarItem>
 <SidebarItem label="Saídas" link="/output">
-    <ArrowDown size={20} />
+    <ArrowDown size={20} slot="icon" />
 </SidebarItem>
 <SidebarItem label="Configurações" link="/settings">
-    <Settings size={20} />
+    <Settings size={20} slot="icon" />
 </SidebarItem>
