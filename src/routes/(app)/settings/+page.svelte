c4c8cd52f4025c0512c67145b225e84f75abc612
Bruno Prado <bwprado@gmail.com>
2023-09-24 01:43:01 -0300

    ✨ Adds `units` to settings

--- src/routes/(app)/settings/+page.svelte
+++ src/routes/(app)/settings/+page.svelte
@@ -5,21 +5,29 @@
     import Modal from "$lib/components/Modal.svelte"
     import Select from "$lib/components/Select.svelte"
     import Table from "$lib/components/Table/Table.svelte"
+
+    import { applyAction, deserialize } from "$app/forms"
     import { Tab, TabGroup, getModalStore, getToastStore } from "@skeletonlabs/skeleton"
-    import { Grid2x2, Trash, Users } from "lucide-svelte"
-    import type { IProfile } from "../../../hooks.server"
+    import type { ActionResult } from "@sveltejs/kit"
+    import { Trash } from "lucide-svelte"
+    import type { Profile } from "../../../types/supabase"
     import type { PageData } from "./$types"
 
     const modal = getModalStore()
     const toast = getToastStore()
 
-    let selectedUser: IProfile | undefined
+    let selectedUser: Profile | undefined
     export let data: PageData
 
     $: showModal = false
+    $: showAddUnitModal = false
     $: selectedUser = undefined
+    $: newUnit = {
+        name: "",
+        acronym: "",
+    }
     $: loading = false
-
+    $: units = data?.units ?? []
     const users = data?.users ?? []
 
     const handleRowClick = (id: string) => {
@@ -70,6 +78,35 @@
     }
 
     let tabSet: number = 0
+
+    const handleAddUnitSubmit = async (e: { currentTarget: EventTarget & HTMLFormElement }) => {
+        const data = new FormData(e.currentTarget)
+
+        const response = await fetch(e.currentTarget.action, {
+            method: "POST",
+            body: data,
+        })
+
+        const result: ActionResult = deserialize(await response.text())
+
+        if (result.type === "success") {
+            units = [...units, result?.data?.body]
+            showAddUnitModal = false
+            toast.trigger({
+                message: "Unidade adicionada com sucesso",
+            })
+        } else {
+            toast.trigger({
+                message: "Erro ao adicionar unidade",
+            })
+        }
+        newUnit = {
+            name: "",
+            acronym: "",
+        }
+        loading = false
+        applyAction(result)
+    }
 </script>
 
 <TabGroup>
@@ -78,11 +115,11 @@
             <span>Usuários</span>
         </div>
     </Tab>
-    <!-- <Tab bind:group={tabSet} name="categories" value={1}>
+    <Tab bind:group={tabSet} name="categories" value={1}>
         <div class="text-sm flex flex-col items-center justify-center gap-y-2">
-            <span>Categorias</span>
+            <span>Unidades</span>
         </div>
-    </Tab> -->
+    </Tab>
     <svelte:fragment slot="panel">
         {#if tabSet === 0}
             <div class="flex flex-col sm:flex-row justify-between items-center pb-10 gap-4">
@@ -102,7 +139,28 @@
                     data={users} />
             </div>
         {:else if tabSet === 1}
-            (tab panel 2 contents)
+            <div class="flex flex-col sm:flex-row justify-between items-center pb-10 gap-4">
+                <div class="flex flex-col">
+                    <h1 class="text-xl font-bold whitespace-nowrap">Unidades de medida</h1>
+                    <p class="text-xs text-surface-400">
+                        Você pode adicionar as unidades de medida necessárias para controlar seu
+                        estoque.
+                    </p>
+                </div>
+                <Button
+                    id="add_unit"
+                    intent="primary"
+                    type="button"
+                    class="w-fit"
+                    on:click={() => (showAddUnitModal = true)}>Adicionar Unidade</Button>
+            </div>
+            <Table
+                columns={[
+                    { label: "Unidade", key: "name", type: "string" },
+                    { label: "Acrônimo", key: "acronym", type: "string" },
+                ]}
+                {handleRowClick}
+                data={units} />
         {:else if tabSet === 2}
             (tab panel 3 contents)
         {/if}
@@ -147,6 +205,48 @@
                     { name: "Usuário", id: "user" },
                 ]}
                 value={selectedUser ? selectedUser?.role : ""} />
+            <Button
+                type="submit"
+                id="save_user"
+                {loading}
+                intent="primary"
+                class="w-full mt-auto"
+                on:click={() => (loading = true)}>Salvar</Button>
+        </form>
+    </svelte:fragment>
+    <svelte:fragment slot="footer">
+        <div></div>
+    </svelte:fragment>
+</Modal>
+
+<Modal
+    headerText="Adicionar Unidade"
+    bind:showModal={showAddUnitModal}
+    confirmFunction={() => console.log("Confirmation modal")}
+    position="right">
+    <svelte:fragment slot="action">
+        {#if selectedUser}
+            <form method="POST" action="?/deleteUser">
+                <IconButton on:click={handleDeleteClick}>
+                    <Trash
+                        class="text-gray-900 dark:text-primary-500 hover:text-primary-500 dark:hover:text-gray-200" />
+                </IconButton>
+            </form>
+        {/if}
+    </svelte:fragment>
+    <svelte:fragment slot="body">
+        <form
+            method="POST"
+            action="?/addUnit"
+            class="flex flex-col h-full gap-y-4"
+            on:submit|preventDefault={handleAddUnitSubmit}>
+            <Input id="unit" name="unit" label="Unidade" type="text" value={newUnit.name} />
+            <Input
+                id="acronym"
+                name="acronym"
+                label="Acrônimo"
+                type="acronym"
+                value={newUnit.acronym} />
             <Button
                 type="submit"
                 id="save_user"
