21d9d44cc5bdaa998b05846030c9fea9e25e65f7
Bruno Prado <bwprado@gmail.com>
2023-09-24 16:30:28 -0300

    ♻️ Changes to `flex-row`

--- src/lib/components/PageHeader.svelte
+++ src/lib/components/PageHeader.svelte
@@ -6,7 +6,7 @@
     export let title: string = ""
 </script>
 
-<section class={twMerge("flex flex-col sm:flex-row justify-between gap-4", className)}>
-    <h1 class="text-xl sm:text-2xl font-bold whitespace-nowrap">{title}</h1>
+<section class={twMerge("flex flex-row justify-between gap-4", className)}>
+    <h1 class="text-lg sm:text-xl font-bold whitespace-nowrap">{title}</h1>
     <slot title="action" />
 </section>
