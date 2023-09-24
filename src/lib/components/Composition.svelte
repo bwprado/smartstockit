47ae7f23893234bf6494d13e9633db774cd1803b
Bruno Prado <bwprado@gmail.com>
2023-09-24 01:38:49 -0300

    💥 Removes individual `CompositionItem` to solve many problems

--- src/lib/components/Composition.svelte
+++ src/lib/components/Composition.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
-    import { twMerge } from "tailwind-merge"
     import Button from "./Button.svelte"
-    import CompositionItem from "./CompositionItem.svelte"
+    import IconButton from "./IconButton.svelte"
+    import Input from "./Input.svelte"
+    import SelectSearch from "./SelectSearch.svelte"
+
     import { isEqual } from "lodash"
+    import { Minus } from "lucide-svelte"
     import { fade } from "svelte/transition"
+    import { twMerge } from "tailwind-merge"
 
     export let label: string = ""
     export let name: string = "input"
@@ -13,12 +17,9 @@
         input?: string
     } = {}
 
-    export let options: { value: string; label: string }[] = []
-    export let composition: { value: string; label: string; amount: number }[] = []
-    let selected: { value: string; label: string } = {
-        value: "",
-        label: "",
-    }
+    export let options: { value: string; label: string; acronym?: string }[] = []
+    export let composition: { value: string; label: string; amount: number; acronym?: string }[] =
+        []
 </script>
 
 <div transition:fade={{ duration: 100 }} class="w-full flex flex-col gap-y-4">
@@ -30,7 +31,7 @@
         )}
         >{label}
     </label>
-    {#if composition.length > 0}
+    {#if composition?.length > 0}
         <table id="raw_material" class="rounded-t-md w-full">
             <thead class="rounded-t-md">
                 <tr class="text-xs text-surface-900 dark:text-surface-200 bg-surface-700 leading-6">
@@ -39,20 +40,52 @@
                 </tr>
             </thead>
             <tbody class="relative">
-                {#each composition as item}
+                {#each composition as item, index}
                     {#key composition.length}
-                        <CompositionItem
-                            on:selection={(e) => {
-                                selected = e.detail
-                            }}
-                            bind:amount={item.amount}
-                            bind:label={item.label}
-                            bind:value={item.value}
-                            {options}
-                            key={item.value}
-                            on:click={() => {
-                                composition = composition.filter((c) => !isEqual(c, item))
-                            }} />
+                        <tr class="relative" id={item.value + index}>
+                            <td class="first:w-3/5">
+                                <SelectSearch
+                                    {options}
+                                    name={item.label + index}
+                                    id={item.value + index}
+                                    bind:inputValue={composition[index].label}
+                                    on:selection={(e) => {
+                                        composition[index].label = e.detail.label
+                                        composition[index].value = e.detail.value
+                                        composition[index].acronym = e.detail.acronym
+                                    }}
+                                    placeholder="Digite o nome do insumo"
+                                    customClasses={{
+                                        wrapper: "w-full",
+                                        input: "w-full rounded-s-md rounded-e-none",
+                                        label: "pb-0",
+                                    }} />
+                            </td>
+                            <td>
+                                <Input
+                                    placeholder="0"
+                                    type="number"
+                                    bind:value={composition[index].amount}
+                                    name={item.label + "amount" + index}
+                                    id={item.label + "amount" + index}
+                                    customClasses={{
+                                        wrapper: "w-full",
+                                        input: "w-full rounded-none",
+                                    }}
+                                    symbol={item.acronym}>
+                                </Input>
+                            </td>
+                            <td class="pl-2 right-0">
+                                <IconButton
+                                    intent="secondary"
+                                    on:click={() => {
+                                        composition = composition.filter((c) => !isEqual(c, item))
+                                    }}
+                                    ><svelte:fragment slot="icon">
+                                        <Minus size={15} />
+                                    </svelte:fragment></IconButton>
+                            </td>
+                        </tr>
                     {/key}
                 {/each}
             </tbody>
