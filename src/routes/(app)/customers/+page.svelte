f2fb863590b5948bb905761f5b84933b2ab0d8cd
Bruno Prado <bwprado@gmail.com>
2023-09-24 04:21:20 -0300

    🔥 Removes test code

--- src/routes/(app)/customers/+page.svelte
+++ src/routes/(app)/customers/+page.svelte
@@ -1,8 +1,4 @@
 <script>
-    import CompositionItem from "$lib/components/CompositionItem.svelte"
-    import Composition from "$lib/components/Composition.svelte"
 </script>
 
-<Composition options={[{ value: "sugar", label: "Açucar" }]}>
-    <CompositionItem />
-</Composition>
+<div>Customers</div>
