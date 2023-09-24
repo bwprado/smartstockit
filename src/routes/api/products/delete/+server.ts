4607f9be0d08f71854eb632fa6143f4062572045
Bruno Prado <bwprado@gmail.com>
2023-09-24 01:44:56 -0300

    🎨 Improve API code to product deletion

--- src/routes/api/products/delete/+server.ts
+++ src/routes/api/products/delete/+server.ts
@@ -1,19 +1,29 @@
-import type { RequestHandler } from "@sveltejs/kit"
+import { json, type RequestHandler } from "@sveltejs/kit"
 
-export const POST: RequestHandler = async ({ locals: { supabase }, url }) => {
-    const id = url.searchParams.get("id")
-    let products
-    if (id) {
-        const { data, error } = await supabase.from("products").delete().eq("id", id).select()
-        products = data
-        
-        if (error) {
-            return new Response(JSON.stringify({ error }), { status: 500 })
-        }
+export const DELETE: RequestHandler = async ({ locals: { supabase, getSession }, url }) => {
+    const session = await getSession()
+
+    if (!session) {
+        return json({ message: "Usuário não autenticado" }, { status: 401 })
     }
 
-    return new Response(JSON.stringify({ success: products ? products[0] : [] }), {
-        status: 202,
-        statusText: "Produto deletado com sucesso",
-    })
+    const id = url.searchParams.get("id")
+
+    if (!id) {
+        return json({ message: "ID não informado" }, { status: 400 })
+    }
+
+    const { data: product, error } = await supabase
+        .from("products")
+        .delete()
+        .eq("id", id)
+        .eq("user", session.user.id)
+        .select()
+        .single()
+
+    if (error) {
+        return json(error, { status: 500 })
+    }
+
+    return json(product, { status: 200, statusText: "Produto removido com sucesso." })
 }
