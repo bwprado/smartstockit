234bfa1dd4f917a645148bb1f2d503b088d56b34
Bruno Prado <bwprado@gmail.com>
2023-09-24 04:21:43 -0300

    🔥 Removes unused `data` export

--- src/routes/(app)/dashboard/+page.svelte
+++ src/routes/(app)/dashboard/+page.svelte
@@ -1,8 +1,6 @@
 <script>
     import Chart from "$lib/components/Chart.svelte"
     import PageHeader from "$lib/components/PageHeader.svelte"
-
-    export let data
 </script>
 
 <div class="flex flex-col gap-y-10 w-full">
