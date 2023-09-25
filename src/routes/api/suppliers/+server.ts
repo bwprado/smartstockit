de606028322f6a8c8673e46c295a5ee0fca5ed7b
Bruno Prado <bwprado@gmail.com>
2023-09-25 20:43:48 -0300

    🗑️ Code might be deprecated soon

--- src/routes/api/suppliers/+server.ts
+++ src/routes/api/suppliers/+server.ts
@@ -23,3 +23,53 @@ export const POST: RequestHandler = async ({ locals: { supabase, getSession }, r
 
     return json({ data }, { status: 202, statusText: "Fornecedor adicionado com sucesso" })
 }
+
+export const DELETE: RequestHandler = async ({ locals: { supabase, getSession }, request }) => {
+    const session = await getSession()
+    if (!session) {
+        console.log("No session, please login to continue.")
+        return json({ error: "Unauthorized" }, { status: 401 })
+    }
+
+    const { id } = await request.json()
+
+    const { data, error: err } = await supabase
+        .from("suppliers")
+        .delete()
+        .eq("id", id)
+        .eq("user", session.user.id)
+        .select()
+        .single()
+
+    if (err) {
+        console.log(err)
+        return json({ error: err }, { status: 500 })
+    }
+
+    return json(data, { status: 202, statusText: "Fornecedor deletado com sucesso" })
+}
+
+export const PUT: RequestHandler = async ({ locals: { supabase, getSession }, request }) => {
+    const session = await getSession()
+    if (!session) {
+        console.log("No session, please login to continue.")
+        return json({ error: "Unauthorized" }, { status: 401 })
+    }
+
+    const { name, id } = await request.json()
+
+    const { data, error } = await supabase
+        .from("suppliers")
+        .update({ name })
+        .eq("id", id)
+        .eq("user", session.user.id)
+        .select()
+        .single()
+
+    if (error) {
+        console.log(error)
+        return json({ error }, { status: 500 })
+    }
+
+    return json(data, { status: 202, statusText: "Fornecedor atualizado com sucesso" })
+}
