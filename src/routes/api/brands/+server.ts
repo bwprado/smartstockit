e3f097fa4374a4f2d7c1ab13e0b91f0c6bb92c65
Bruno Prado <bwprado@gmail.com>
2023-09-25 20:43:29 -0300

    🗑️ API might be removed in the future

--- src/routes/api/brands/+server.ts
+++ src/routes/api/brands/+server.ts
@@ -1,4 +1,4 @@
-import type { RequestHandler } from "@sveltejs/kit"
+import { json, type RequestHandler } from "@sveltejs/kit"
 
 export const POST: RequestHandler = async ({ locals: { supabase, getSession }, request }) => {
     const session = await getSession()
@@ -17,11 +17,61 @@ export const POST: RequestHandler = async ({ locals: { supabase, getSession }, r
 
     if (error) {
         console.log(error)
-        return new Response(JSON.stringify({ error }), { status: 500 })
+        return json({ error }, { status: 500 })
     }
 
-    return new Response(JSON.stringify(data), {
+    return json(data, {
         status: 202,
         statusText: "Marca adicionada com sucesso",
     })
+}
+
+export const DELETE: RequestHandler = async ({ locals: { supabase, getSession }, request }) => {
+    const session = await getSession()
+    if (!session) {
+        console.log("No session, please login to continue.")
+        return json({ error: "Unauthorized" }, { status: 401 })
+    }
+
+    const { id } = await request.json()
+
+    const { data, error: err } = await supabase
+        .from("brands")
+        .delete()
+        .eq("id", id)
+        .eq("user", session.user.id)
+        .select()
+        .single()
+
+    if (err) {
+        console.log(err)
+        return json({ error: err }, { status: 500 })
+    }
+
+    return json(data, { status: 202, statusText: "Marca deletada com sucesso" })
+}
+
+export const PUT: RequestHandler = async ({ locals: { supabase, getSession }, request }) => {
+    const session = await getSession()
+    if (!session) {
+        console.log("No session, please login to continue.")
+        return json({ error: "Unauthorized" }, { status: 401 })
+    }
+
+    const { name, id } = await request.json()
+
+    const { data, error } = await supabase
+        .from("brands")
+        .update({ name })
+        .eq("id", id)
+        .eq("user", session.user.id)
+        .select()
+        .single()
+
+    if (error) {
+        console.log(error)
+        return json({ error }, { status: 500 })
+    }
+
+    return json(data, { status: 202, statusText: "Marca atualizada com sucesso" })
 }
