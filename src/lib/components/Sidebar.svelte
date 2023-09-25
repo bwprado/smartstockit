bc5d299366fdc73018294388c862228b90c4dc1c
Bruno Prado <bwprado@gmail.com>
2023-09-25 20:40:44 -0300

    🎨 Adds `dark` support

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <nav
-    class="h-full w-0 sm:w-fit transition-all duration-300 my-auto p-2 sm:p-8 hidden lg:block bg-surface-800">
+    class="h-full w-0 sm:w-fit transition-all duration-300 my-auto p-2 sm:p-8 hidden lg:block bg-surface-200 dark:bg-surface-800">
     <ul class="flex-col gap-y-4 flex">
         <slot />
     </ul>
