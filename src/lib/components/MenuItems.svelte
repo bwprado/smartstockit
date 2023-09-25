11afac55fc01e9a36e375a506ac575a46bdbc333
Bruno Prado <bwprado@gmail.com>
2023-09-25 20:39:48 -0300

    🎨 Stores `expanded` state instead of `pathname`

--- src/lib/components/MenuItems.svelte
+++ src/lib/components/MenuItems.svelte
@@ -1,24 +1,23 @@
 <script>
     import { ArrowDown, ArrowUp, Box, LayoutDashboard, Settings, UserSquare2 } from "lucide-svelte"
-    import { page } from "$app/stores"
 
     import SidebarItem from "./SidebarItem.svelte"
     import SidebarSubItem from "./SidebarSubItem.svelte"
 
-    const inventoryLinks = ["/products", "/categories", "/brands", "/suppliers"]
+    $: expanded = false
 </script>
 
 <SidebarItem label="Dashboard" link="/dashboard">
     <LayoutDashboard size={20} slot="icon" />
 </SidebarItem>
-<SidebarItem label="Inventário" link="/products" expanded={inventoryLinks.includes($page.url.pathname)}>
+<SidebarItem label="Inventário" link="/products" {expanded}>
     <Box size={20} slot="icon" />
     <svelte:fragment slot="subitem">
         <SidebarSubItem label="Produtos" link="/products" />
         <SidebarSubItem label="Categorias" link="/categories" />
         <SidebarSubItem label="Marcas" link="/brands" />
         <SidebarSubItem label="Fornecedores" link="/suppliers" />
-        <SidebarSubItem label="Unidades de Medida" link="/units" />
+        <SidebarSubItem label="Unidades" link="/units" />
     </svelte:fragment>
 </SidebarItem>
 <SidebarItem label="Clientes" link="/customers">
