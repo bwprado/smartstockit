6639d521a034691387fdfb4d58e846efe2c68081
Bruno Prado <bwprado@gmail.com>
2023-09-25 20:41:31 -0300

    🎨 Improve code with `cva`

--- src/lib/components/SidebarSubItem.svelte
+++ src/lib/components/SidebarSubItem.svelte
@@ -1,17 +1,27 @@
 <script lang="ts">
     import { page } from "$app/stores"
-    import { CornerDownRight } from "lucide-svelte"
+    import { cx } from "class-variance-authority"
 
     export let label: string
     export let link: string
     $: active = $page.url.pathname === link
+    const style = cx([
+        "flex",
+        "px-6",
+        "py-2",
+        "gap-x-4",
+        "text-surface-400",
+        "dark:text-surface-300",
+        "rounded-lg",
+        "hover:text-secondary-500",
+        "data-[active=true]:dark:text-primary-500",
+        "data-[active=true]:text-primary-500",
+        "data-[active=true]:font-bold",
+    ])
 </script>
 
 <li>
-    <a
-        href={link}
-        data-active={active}
-        class="flex px-6 py-2 gap-x-4 text-surface-300 rounded-lg hover:text-secondary-500 data-[active=true]:text-secondary-500 data-[active=true]:font-bold">
+    <a href={link} data-active={active} class={style}>
         <span>{label}</span>
     </a>
 </li>
