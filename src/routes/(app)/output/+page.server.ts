b8b86816afc8d2626cafb675722a9267469f422c
Bruno Prado <bwprado@gmail.com>
2023-09-26 11:53:16 -0300

    ♻️ Updates code pattern to async functions return

--- src/routes/(app)/output/+page.server.ts
+++ src/routes/(app)/output/+page.server.ts
@@ -1,40 +1,66 @@
-import { redirect, type Actions } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types.js';
+import { redirect, type Actions, error } from "@sveltejs/kit"
+import type { PageServerLoad } from "./$types.js"
+import type { Product } from "../../../types/supabase.js"
 
 export const load: PageServerLoad = async ({ locals: { getSession, supabase } }) => {
-    if (!await getSession()) {
-        throw redirect(303, '/login')
+    const session = await getSession()
+    if (!session) {
+        throw redirect(303, "/login")
     }
 
-    const { data: outputs, error: err } = await supabase
-        .from('inventory')
-        .select('*, products(name)')
-        .lt('amount', 0)
-        .order('created_at', { ascending: false })
+    const fetchOutputs = async () => {
+        const { data: outputs, error: err } = await supabase
+            .from("inventory")
+            .select("*, products(name)")
+            .lt("amount", 0)
+            .order("created_at", { ascending: false })
+            .eq("user", session?.user?.id)
 
-    if (err) {
-        throw err
+        if (err) {
+            throw error(500, "Erro ao buscar saídas do estoque")
+        }
+
+        return outputs.map((input) => ({ ...input, productName: input.products.name }))
+    }
+
+    const fetchProducts = async () => {
+        const { data: products, error: err } = await supabase
+            .from("products")
+            .select()
+            .eq("user", session?.user?.id)
+
+        if (err) {
+            throw error(500, "Erro ao buscar produtos")
+        }
+
+        return products as Product[]
     }
 
     return {
-        outputs: outputs.map(input => ({ ...input, productName: input.products.name }))
+        outputs: await fetchOutputs(),
+        products: await fetchProducts(),
     }
-};
-
+}
 
 export const actions: Actions = {
     default: async ({ request, locals: { supabase, getSession } }) => {
+        console.log("default")
         const session = await getSession()
         const outputData = Object.fromEntries(await request.formData())
 
         const negativeAmount = -Number(outputData.amount)
-        const { data, error: err } = await supabase.from('inventory').insert([{ ...outputData, amount: negativeAmount, user: session?.user?.id }])
+        const { data, error: err } = await supabase
+            .from("inventory")
+            .insert([{ ...outputData, amount: negativeAmount, user: session?.user?.id, price: 0 }])
+            .eq("user", session?.user?.id)
+            .select()
+            .single()
 
         if (err) {
             console.log(err)
             return { status: 500, body: "Erro ao fazer retirada do estoque" }
         }
 
-        return { status: 303, body: "Produto retirado com sucesso." }
-    }
-};
+        return { status: 303, body: "Produto retirado com sucesso.", data }
+    },
+}
