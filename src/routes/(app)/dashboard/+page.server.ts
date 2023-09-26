235507ac1e96af371f6edae64c4c8e98e37aadce
Bruno Prado <bwprado@gmail.com>
2023-09-26 18:56:14 -0300

    ♻️ Updates `load` function to check for login

--- src/routes/(app)/dashboard/+page.server.ts
+++ src/routes/(app)/dashboard/+page.server.ts
@@ -1,8 +1,13 @@
-import { redirect } from "@sveltejs/kit";
-import type { PageServerLoad } from "./$types";
+import { redirect } from "@sveltejs/kit"
+import type { PageServerLoad } from "./$types"
 
 export const load: PageServerLoad = async ({ locals: { getSession } }) => {
-    if (!await getSession()) {
-        throw redirect(303, '/login')
+    const session = await getSession()
+
+    if (!session) {
+        throw redirect(303, "/login")
     }
-};
+    return {
+        session,
+    }
+}
