aab33740a364de26dd37da573aa0e02384d07104
Bruno Prado <bwprado@gmail.com>
2023-09-26 01:12:22 -0300

    🎨 Improve code, error handling and brings data

--- src/routes/(app)/input/+page.server.ts
+++ src/routes/(app)/input/+page.server.ts
@@ -1,26 +1,38 @@
-import { redirect, type Actions } from "@sveltejs/kit";
-import type { PageServerLoad } from "./$types";
+import { redirect, type Actions, error } from "@sveltejs/kit"
+import type { PageServerLoad } from "./$types"
 
 export const load: PageServerLoad = async ({ locals: { getSession, supabase } }) => {
-    if (!await getSession()) {
-        throw redirect(303, '/login')
+    if (!(await getSession())) {
+        throw redirect(303, "/login")
     }
 
     const { data: inputs, error: err } = await supabase
-        .from('inventory')
-        .select('*, products(name)')
-        .gt('amount', 0)
-        .order('created_at', { ascending: false })
+        .from("inventory")
+        .select("*, products(name)")
+        .gt("amount", 0)
+        .order("created_at", { ascending: false })
 
     if (err) {
         console.error(err)
-        throw err
+        throw error(500, "Erro ao carregar entradas de inventório")
+    }
+
+    const fetchProducts = async () => {
+        const { data, error: err } = await supabase.from("products").select("id, name")
+
+        if (err) {
+            console.error(err)
+            throw error(500, "Erro ao carregar produtos")
+        }
+
+        return data
     }
 
     return {
-        inputs: inputs.map(input => ({ ...input, productName: input.products.name }))
+        inputs: inputs.map((input) => ({ ...input, productName: input.products.name })),
+        products: await fetchProducts(),
     }
-};
+}
 
 export const actions: Actions = {
     default: async ({ request, locals: { supabase } }) => {
@@ -29,8 +41,14 @@ export const actions: Actions = {
         inputData?.fresh && delete inputData.fresh
         const price = +inputData?.price
         const amount = +inputData?.amount
-        const expiration_date = inputData?.expiration_date === "" && null
-        const { data, error: err } = await supabase.from('inventory').insert([{ ...inputData, amount, price, expiration_date }])
+        const expiration_date =
+            inputData?.expiration_date === "" ? null : inputData?.expiration_date
+        console.log(expiration_date)
+        const { data, error: err } = await supabase
+            .from("inventory")
+            .insert([{ ...inputData, amount, price, expiration_date }])
+            .select()
+            .single()
 
         if (err) {
             console.log(err)
@@ -38,5 +56,5 @@ export const actions: Actions = {
         }
 
         return { status: 200, body: "Entrada de inventório feita com sucesso" }
-    }
+    },
 }
