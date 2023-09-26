cb9b1da6dc5739002d1292fcbd253c90dc5f6f20
Bruno Prado <bwprado@gmail.com>
2023-09-26 14:57:18 -0300

    🐛 Fixes Google login bug adding missing redirect

--- src/routes/auth/callback/+server.ts
+++ src/routes/auth/callback/+server.ts
@@ -1,11 +1,18 @@
-import { redirect } from '@sveltejs/kit'
+import { error, redirect, type RequestHandler } from "@sveltejs/kit"
 
-export const GET = async ({ url, locals: { supabase } }) => {
-  const code = url.searchParams.get('code')
+export const GET: RequestHandler = async ({ url, locals: { supabase } }) => {
+    const code = url.searchParams.get("code")
 
-  if (code) {
-    await supabase.auth.exchangeCodeForSession(code)
-  }
+    if (code) {
+        const { data, error: err } = await supabase.auth.exchangeCodeForSession(code)
 
-  throw redirect(303, '/')
+        if (err) {
+            console.error(err)
+            throw error(401, "/login")
+        }
+
+        throw redirect(303, "/dashboard")
+    }
+
+    throw redirect(304, "/login")
 }
