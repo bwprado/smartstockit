824efe41e35e43174db5c9529e113d86fcfab555
Bruno Prado <bwprado@gmail.com>
2023-09-27 16:19:47 -0300

    ♻️ Makes code work now

--- src/routes/api/categories/+server.ts
+++ src/routes/api/categories/+server.ts
@@ -20,7 +20,7 @@ export const PUT: RequestHandler = async ({ locals: { supabase, getSession }, re
         throw error(500, { message: err.message })
     }
 
-    return new Response(JSON.stringify(data), {
+    return json(data, {
         status: 200,
         statusText: "Categoria atualizada com sucesso",
     })
@@ -34,17 +34,19 @@ export const POST: RequestHandler = async ({ request, locals: { supabase, getSes
         throw error(401, { message: "Unauthorized" })
     }
 
-    const { name, ref } = (await request.json()) || {}
+    const { name } = (await request.json()) || {}
 
     const { data, error: err } = await supabase
         .from("categories")
-        .insert({ name, user: session.user?.id, ref })
+        .insert({ name, user: session.user?.id })
+        .eq("user", session.user?.id)
         .select()
         .single()
 
     if (err) {
-        return new Response(JSON.stringify({ err }), { status: 500 })
+        console.log(err)
+        return json({ err }, { status: 500 })
     }
 
-    return json({ data }, { status: 202, statusText: "Categoria adicionada com sucesso" })
+    return json(data, { status: 202, statusText: "Categoria adicionada com sucesso" })
 }
