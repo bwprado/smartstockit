78d56cddb78440a576ecd5c05043f9ae5b3d3c7b
Bruno Prado <bwprado@gmail.com>
2023-09-27 08:39:23 -0300

    ♻️ Changes to `localhost`

--- src/routes/(auth)/login/+page.server.ts
+++ src/routes/(auth)/login/+page.server.ts
@@ -3,18 +3,17 @@ import { fail, redirect } from "@sveltejs/kit"
 import type { Actions } from "./$types"
 
 export const actions: Actions = {
-    default: async ({ request, locals: { supabase, getSession }, url }) => {
+    default: async ({ request, locals: { supabase }, url }) => {
         const provider = url.searchParams.get("provider") as Provider
 
         if (provider === "google") {
             const { data, error } = await supabase.auth.signInWithOAuth({
                 provider,
                 options: {
-                    redirectTo: `https://smartstockit.com/auth/callback/`,
+                    redirectTo: "http://localhost:5173/auth/callback/",
                 },
             })
 
-            console.log(data)
             if (error instanceof AuthApiError) {
                 console.log(error)
                 return fail(error.status, { message: error.message })
