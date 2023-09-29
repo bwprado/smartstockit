5663fe143b1c110ea0ad84fdd5cbda54f6338788
Bruno Prado <bwprado@gmail.com>
2023-09-28 23:48:12 -0300

    ♻️ Major refactor changing from form actions to api requests

--- src/routes/(app)/input/+page.svelte
+++ src/routes/(app)/input/+page.svelte
@@ -7,20 +7,19 @@
     import SelectSearch from "$lib/components/SelectSearch.svelte"
     import Table from "$lib/components/Table/Table.svelte"
 
-    import { getToastStore } from "@skeletonlabs/skeleton"
-    import type { ActionData, PageServerData } from "./$types"
-    import { isEmpty } from "lodash"
-    import { format, parseISO } from "date-fns"
     import IconButton from "$lib/components/IconButton.svelte"
+    import { getModalStore, getToastStore } from "@skeletonlabs/skeleton"
+    import { format, parseISO } from "date-fns"
     import { Trash } from "lucide-svelte"
+    import type { PageServerData } from "./$types"
 
     const toast = getToastStore()
+    const modal = getModalStore()
 
     let selectedProduct: any = {}
     let selectedInput: any = {}
 
     export let data: PageServerData
-    export let form: ActionData
 
     const searchableProducts = data.products.map((product) => ({
         label: product.name,
@@ -29,25 +28,54 @@
     }))
 
     $: showModal = false
-    $: isFresh = false
     $: loading = false
 
-    const handleChange = (event: Event) => {
-        if (event?.target instanceof HTMLInputElement) {
-            isFresh = event?.target?.checked
-        }
-    }
-
-    if (form?.status) {
-        if (form?.status === 500) {
+    const handleSubmit = async () => {
+        if (!selectedProduct.id) {
+            showModal = false
             toast.trigger({
-                message: form.body,
+                message: "Selecione um produto",
                 background: "bg-error-500",
             })
+            return
+        }
+
+        if (!selectedInput.amount) {
+            showModal = false
+            toast.trigger({
+                message: "Insira uma quantidade",
+                background: "bg-error-500",
+            })
+            return
+        }
+
+        loading = true
+        const res = await fetch("/api/input", {
+            method: "POST",
+            body: JSON.stringify({
+                ...selectedInput,
+                product: selectedProduct.id,
+                fresh: !!selectedInput?.fresh,
+            }),
+        })
+
+        if (res.ok) {
+            const input = await res.json()
+            const product = data.products.find((product) => product.id === input.product)
+            data.inputs = [...data.inputs, { ...input, productName: product?.name }]
+
+            toast.trigger({
+                message: "Entrada adicionada com sucesso",
+            })
+
+            loading = false
+            showModal = false
         } else {
             toast.trigger({
-                message: form.body,
+                message: res.statusText,
+                background: "bg-error-500",
             })
+            loading = false
         }
     }
 
@@ -60,19 +88,65 @@
 
     const handleRowClick = (id: string) => {
         selectedInput = data.inputs.find((input) => input.id === id)
+        selectedProduct = data.products.find((product) => product.id === selectedInput.product)
+        selectedInput.expiration_date = selectedInput.expiration_date
+            ? format(parseISO(selectedInput.expiration_date), "yyyy-MM-dd")
+            : ""
         showModal = true
     }
 
-    const handleDeleteClick = async () => {}
+    const handleDeleteClick = async () => {
+        const deleteId = selectedInput.id
+        showModal = false
+        modal.trigger({
+            type: "confirm",
+            title: "Excluir Entrada",
+            body: "Você tem certeza que deseja excluir essa entrada?",
+            buttonTextConfirm: "Excluir",
+            buttonTextCancel: "Cancelar",
+            response: async (r) => {
+                if (!r) return
+
+                loading = true
+                const res = await fetch("api/input", {
+                    method: "DELETE",
+                    body: JSON.stringify(deleteId),
+                })
+
+                if (res.ok) {
+                    const deleted = await res.json()
+
+                    data.inputs = data.inputs.filter((input) => input.id !== deleted.id)
+
+                    toast.trigger({
+                        message: "Entrada excluída com sucesso",
+                    })
+                } else {
+                    toast.trigger({
+                        message: res.statusText,
+                        background: "bg-error-500",
+                    })
+                }
+                showModal = false
+                loading = false
+            },
+        })
+    }
 </script>
 
 <PageHeader title="Entradas">
-    <Button class="w-fit" on:click={() => (showModal = true)}>Adicionar Entrada</Button>
+    <Button
+        class="w-fit"
+        on:click={() => {
+            showModal = true
+            selectedInput = {}
+            selectedProduct = {}
+        }}>Adicionar Entrada</Button>
 </PageHeader>
 
 <EmptyWrapper
     title="Sem dados"
-    message="Você ainda não realizou nenhuma saída, clique no botão cima para fazê-lo"
+    message="Você ainda não realizou nenhuma entrada, clique no botão cima para fazê-lo"
     length={data.inputs.length}>
     <section class="table-container rounded-lg" slot="content">
         <Table
@@ -90,7 +164,7 @@
 
 <Modal
     bind:showModal
-    headerText={isEmpty(selectedInput) ? "Entrada de Produtos" : "Alterar Entrada de Produto"}
+    headerText={selectedInput.id ? "Alterar Entrada de Produto" : "Entrada de Produtos"}
     on:close={() => {
         selectedInput = {}
         selectedProduct = {}
@@ -106,12 +180,12 @@
     </svelte:fragment>
 
     <svelte:fragment slot="body">
-        <form method="POST" class="flex flex-col gap-y-4 h-full">
+        <div class="flex flex-col gap-y-4 h-full">
             <SelectSearch
                 label="Produto"
                 options={searchableProducts}
                 on:selection={handleProductSelection}
-                inputValue={selectedProduct.name || selectedInput?.productName || ""}
+                bind:inputValue={selectedProduct.name}
                 id="product"
                 name="product" />
             <Input
@@ -119,36 +193,32 @@
                 id="amount"
                 type="btn-number"
                 label="Quantidade"
-                value={selectedInput?.amount + "" || ""} />
+                bind:value={selectedInput.amount} />
             <Input
-                value={selectedInput?.price || ""}
                 name="price"
                 id="price"
                 type="number"
                 step="0.01"
                 label="Preço"
+                bind:value={selectedInput.price}
                 symbol={{ position: "left", text: "R$" }} />
             <Input
-                value={selectedInput.fresh || false}
                 type="checkbox"
                 name="fresh"
                 id="fresh"
                 label="Produto Fresco"
-                on:change={handleChange} />
+                bind:checked={selectedInput.fresh} />
             <Input
-                value={selectedInput.expiration_date
-                    ? format(parseISO(selectedInput.expiration_date), "yyyy-MM-dd")
-                    : ""}
+                bind:value={selectedInput.expiration_date}
                 type="date"
                 id="expiration_date"
                 label="Data de Vencimento"
                 name="expiration_date"
-                disabled={isFresh} />
-            <Button type="submit" class="mt-auto w-full" on:click={() => (loading = true)} {loading}
-                >Adicionar Entrada</Button>
-        </form>
+                bind:disabled={selectedInput.fresh} />
+        </div>
     </svelte:fragment>
     <svelte:fragment slot="footer">
-        <div></div>
+        <Button type="submit" class="mt-auto w-full" on:click={handleSubmit} {loading}
+            >Adicionar Entrada</Button>
     </svelte:fragment>
 </Modal>
