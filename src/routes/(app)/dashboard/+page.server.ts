8022e75dc6206584c65c75824be008e4c697361e
Bruno Prado <bwprado@gmail.com>
2023-09-30 17:50:03 -0300

    ✨ Adds data to dashboard

--- src/routes/(app)/dashboard/+page.server.ts
+++ src/routes/(app)/dashboard/+page.server.ts
@@ -1,10 +1,37 @@
-import { redirect } from "@sveltejs/kit"
+import { fail, redirect } from "@sveltejs/kit"
 import type { PageServerLoad } from "./$types"
 
 export const load: PageServerLoad = async ({ locals: { supabase, getSession } }) => {
-    const session = await getSession()
+    const fetchInventory = async () => {
+        const session = await getSession()
 
-    if (!session) {
-        throw redirect(303, "/login")
+        if (!session) {
+            throw redirect(303, "/login")
+        }
+
+        const { data: input, error: errInput } = await supabase
+            .from("inventory")
+            .select("*, products!inner(name, price)")
+            .gte("amount", 1)
+            .eq("user", session.user.id)
+
+        const { data: output, error: errOutput } = await supabase
+            .from("inventory")
+            .select("*, products!inner(name, price)")
+            .lte("amount", -1)
+            .eq("user", session.user.id)
+
+        if (errInput || errOutput) {
+            console.log(errInput, errOutput)
+            throw fail(500, { message: "Alguma coisa deu errado!" })
+        }
+        return {
+            input,
+            output,
+        }
+    }
+
+    return {
+        inventory: await fetchInventory(),
     }
 }
