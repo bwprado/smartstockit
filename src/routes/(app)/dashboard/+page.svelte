3e8a5ec6b728703be916d4e9d556ea5c48ed5986
Bruno Prado <bwprado@gmail.com>
2023-09-30 17:50:17 -0300

    🗃️ Changes to show chart

--- src/routes/(app)/dashboard/+page.svelte
+++ src/routes/(app)/dashboard/+page.svelte
@@ -1,9 +1,49 @@
-<script>
+<script lang="ts">
     import Chart from "$lib/components/Chart.svelte"
     import PageHeader from "$lib/components/PageHeader.svelte"
+    import type { PageServerData } from "./$types"
+
+    export let data: PageServerData
+
+    $: groupedInput = data.inventory.input.reduce(
+        (acc, item) => {
+            if (acc[item.products.name]) {
+                acc[item.products.name] = {
+                    amount: acc[item.products.name].amount + item.amount,
+                    price: Math.round(acc[item.products.name].price + item.price),
+                }
+            } else {
+                acc[item.products.name] = {
+                    amount: item.amount,
+                    price: Math.round(item.price),
+                }
+            }
+            return acc
+        },
+        {} as Record<string, typeof data.inventory>,
+    )
+    $: groupedOutput = data.inventory.output.reduce(
+        (acc, item) => {
+            if (acc[item.products.name]) {
+                acc[item.products.name] = {
+                    amount: acc[item.products.name].amount + item.amount,
+                    price: Math.round(acc[item.products.name].price + item.price),
+                }
+            } else {
+                acc[item.products.name] = {
+                    amount: item.amount,
+                    price: Math.round(item.price),
+                }
+            }
+            return acc
+        },
+        {} as Record<string, typeof data.inventory>,
+    )
 </script>
 
 <div class="flex flex-col gap-y-10 w-full">
     <PageHeader title="Dashboard"></PageHeader>
-    <Chart />
+    <!-- <Chart labels={Object.keys(groupedInput)} datasets={Object} /> -->
+    {JSON.stringify(groupedInput)}
+    {JSON.stringify(groupedOutput)}
 </div>
