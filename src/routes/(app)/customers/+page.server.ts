98fd1eef6bc9072c77ce3e7776f398afb1e8214d
Bruno Prado <bwprado@gmail.com>
2023-10-01 13:02:37 -0300

    ♻️ Changes to form `Actions`

--- src/routes/(app)/customers/+page.server.ts
+++ src/routes/(app)/customers/+page.server.ts
@@ -1,3 +1,4 @@
+import { fail, type Actions } from "@sveltejs/kit"
 import type { Customer } from "../../../types/supabase"
 import type { PageServerLoad } from "./$types"
 
@@ -24,5 +25,69 @@ export const load: PageServerLoad = async ({ locals: { supabase, getSession } })
 
     return {
         customers: await fetchCustomers(),
+    }
+}
+
+export const actions: Actions = {
+    add: async ({ locals: { supabase, getSession }, request }) => {
+        const session = await getSession()
+        if (!session) {
+            return fail(401, { message: "Usuário não autenticado", noSession: true })
+        }
+
+        const body = await request.formData()
+
+        const name = body.get("name")
+        const email = body.get("email")
+        const phone = body.get("phone")
+        const address = body.get("address")
+
+        if (!name) {
+            return fail(400, { message: "Nome é obrigatório", nameMissing: true })
+        }
+
+        await supabase.from("customers").insert({
+            name,
+            email,
+            phone,
+            address,
+            user: session.user.id,
+        })
+
+        return {
+            success: true,
+        }
+    },
+    edit: async ({ locals: { supabase, getSession }, request }) => {
+        const session = await getSession()
+        if (!session) {
+            return fail(401, { message: "Usuário não autenticado", noSession: true })
+        }
+
+        const body = await request.formData()
+
+        const name = body.get("name")
+        const email = body.get("email")
+        const phone = body.get("phone")
+        const address = body.get("address")
+        const id = body.get("id")
+
+        if (!name) {
+            return fail(400, { message: "Nome é obrigatório", nameMissing: true })
+        }
+
+        await supabase
+            .from("customers")
+            .update({
+                name,
+                email,
+                phone,
+                address,
+            })
+            .eq("id", id)
+
+        return {
+            success: true,
+        }
     }
 }
