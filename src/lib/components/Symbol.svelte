3f0b96f59bf3164d00af7c15bcbe520d1a099f0a
Bruno Prado <bwprado@gmail.com>
2023-10-03 00:22:31 -0300

    ✨ New component for symbol on input

---
+++ src/lib/components/Symbol.svelte
@@ -0,0 +1,7 @@
+<script>
+    export let text = ""
+</script>
+
+<p class="p-2 dark:bg-surface-700 dark:text-surface-300 bg-surface-100 text-surface-400 text-xs h-full flex items-center rounded-s-md">
+    {text}
+</p>
