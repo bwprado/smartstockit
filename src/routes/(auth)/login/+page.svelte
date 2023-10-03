29b9fda8401c0013331edc5b8283e2dbf6095a5a
Bruno Prado <bwprado@gmail.com>
2023-10-03 09:09:21 -0300

    💄 Updates text and grids

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -7,8 +7,8 @@
     class="grid grid-rows-3 w-screen h-screen overflow-hidden items-center justify-center hero-gradient">
     <div></div>
     <div
-        class="w-[350px] sm:w-[500px] items-center flex flex-col gap-y-6 justify-center rounded-lg border dark:border-surface-500 bg-surface-800 p-10 sm:p-20">
-        <form method="post" class="flex flex-col gap-y-6 items-center justify-center w-full">
+        class="w-[300px] sm:w-[500px] items-center flex flex-col gap-y-6 justify-center rounded-lg border dark:border-surface-500 bg-surface-800 p-10 sm:p-20">
+        <form method="post" class="flex flex-col gap-y-4 items-center justify-center w-full">
             <h1 class="font-bold text-2xl">Smart Stock It</h1>
             <p class="font-bold">Login</p>
             <Input id="email" name="email" label="Email" type="email" />
@@ -17,7 +17,7 @@
             <div
                 class="grid grid-cols-[1fr,max-content,1fr] items-center justify-center w-full gap-x-4">
                 <div class="border-b-[1px] border-surface-400" />
-                <p class="uppercase text-xs text-surface-400">ou continue com</p>
+                <p class="uppercase text-[10px] text-surface-400">ou continue com</p>
                 <div class="border-b-[1px] border-surface-400" />
             </div>
             <button
