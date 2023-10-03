b47f11db56d854a09017fc6cf7f242f46217d720
Bruno Prado <bwprado@gmail.com>
2023-10-03 00:21:23 -0300

    💄 changes to bg

--- src/lib/components/MobileSidebar.svelte
+++ src/lib/components/MobileSidebar.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <nav
-    class="bg-gray-200 dark:bg-surface-800 h-full flex flex-col gap-y-20 w-full transition-all duration-300 p-8 rounded-lg">
+    class="bg-surface-200 dark:bg-surface-800 h-full flex flex-col gap-y-20 w-full transition-all duration-300 p-8 rounded-lg">
     <ul class="flex-col gap-y-4 flex">
         <slot />
     </ul>
