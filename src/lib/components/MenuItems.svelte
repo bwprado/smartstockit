cb54151074a3cc653d0417dffbf18c02b2ffbab6
Bruno Prado <bwprado@gmail.com>
2023-10-08 11:58:06 -0300

    ♻️ Major changes to component, adds folders to products

--- src/lib/components/MenuItems.svelte
+++ src/lib/components/MenuItems.svelte
@@ -1,5 +1,6 @@
 <script>
-    import { ArrowDown, ArrowUp, Box, LayoutDashboard, Settings, UserSquare2 } from "lucide-svelte"
+    import { Box, Clipboard, LayoutDashboard, Settings, UserSquare2 } from "lucide-svelte"
+    import { page } from "$app/stores"
 
     import SidebarItem from "./SidebarItem.svelte"
     import SidebarSubItem from "./SidebarSubItem.svelte"
@@ -10,24 +11,28 @@
 <SidebarItem label="Dashboard" link="/dashboard">
     <LayoutDashboard size={20} slot="icon" />
 </SidebarItem>
-<SidebarItem label="Inventário" link="/products" {expanded}>
+<SidebarItem label="Inventário" link="/inventory" expanded={expanded || $page.url.pathname.includes("/inventory")}>
     <Box size={20} slot="icon" />
     <svelte:fragment slot="subitem">
-        <SidebarSubItem label="Produtos" link="/products" />
-        <SidebarSubItem label="Categorias" link="/categories" />
-        <SidebarSubItem label="Marcas" link="/brands" />
-        <SidebarSubItem label="Fornecedores" link="/suppliers" />
-        <SidebarSubItem label="Unidades" link="/units" />
+        <SidebarSubItem label="Entradas" link="/inventory/input" />
+        <SidebarSubItem label="Saídas" link="/inventory/output" />
+    </svelte:fragment>
+</SidebarItem>
+<SidebarItem
+    label="Cadastros"
+    link="/registration"
+    expanded={expanded || $page.url.pathname.includes("/registration")}>
+    <Clipboard size={20} slot="icon" />
+    <svelte:fragment slot="subitem">
+        <SidebarSubItem label="Produtos" link="/registration/products" />
+        <SidebarSubItem label="Categorias" link="/registration/categories" />
+        <SidebarSubItem label="Marcas" link="/registration/brands" />
+        <SidebarSubItem label="Fornecedores" link="/registration/suppliers" />
+        <SidebarSubItem label="Unidades" link="/registration/units" />
     </svelte:fragment>
 </SidebarItem>
 <SidebarItem label="Clientes" link="/customers">
     <UserSquare2 size={20} slot="icon" />
-</SidebarItem>
-<SidebarItem label="Entradas" link="/input">
-    <ArrowUp size={20} slot="icon" />
-</SidebarItem>
-<SidebarItem label="Saídas" link="/output">
-    <ArrowDown size={20} slot="icon" />
 </SidebarItem>
 <SidebarItem label="Configurações" link="/settings">
     <Settings size={20} slot="icon" />
