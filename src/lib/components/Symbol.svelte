14d02f894f092415818659e72f2bffd784eca235
Bruno Prado <bwprado@gmail.com>
2023-10-08 11:59:33 -0300

    💄 Major changes to component styling

--- src/lib/components/Symbol.svelte
+++ src/lib/components/Symbol.svelte
@@ -1,7 +1,25 @@
 <script>
+    import { cx } from "class-variance-authority"
+    import { twMerge } from "tailwind-merge"
+
+    let customClass = ""
     export let text = ""
+    export { customClass as class }
+
+    const pStyle = cx([
+        "p-2",
+        "dark:bg-surface-700",
+        "dark:text-surface-300",
+        "bg-surface-100",
+        "text-surface-400",
+        "text-xs",
+        "h-full",
+        "flex",
+        "items-center",
+        "rounded-s-md",
+    ])
 </script>
 
-<p class="p-2 dark:bg-surface-700 dark:text-surface-300 bg-surface-100 text-surface-400 text-xs h-full flex items-center rounded-s-md">
+<p class={twMerge(pStyle, customClass)}>
     {text}
 </p>
