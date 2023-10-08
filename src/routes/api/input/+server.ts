d9b962cc741a8d79dd418253516a4e7c4c6e42c0
Bruno Prado <bwprado@gmail.com>
2023-10-08 12:03:23 -0300

    ✨ Adds bulk `insert` for raw products removal.

--- src/routes/api/input/+server.ts
+++ src/routes/api/input/+server.ts
@@ -7,19 +7,38 @@ export const POST: RequestHandler = async ({ locals: { supabase, getSession }, r
     }
     const { fresh, ...input } = await request.json()
 
+    const { data: product, error: errProduct } = await supabase
+        .from("products")
+        .select("composition")
+        .eq("id", input.product)
+        .single()
+
+    if (errProduct) {
+        console.log(errProduct)
+        throw error(500, { message: `Erro ao buscar produto - ${errProduct?.message}` })
+    }
+
+    const insert = [
+        { ...input, amount: +input.amount, user: session?.user?.id, price: +input.price || 0 },
+        ...product.composition.map((item: any) => ({
+            product: item.value,
+            price: item?.price || 0,
+            amount: -item.amount,
+            user: session?.user?.id,
+        })),
+    ]
+
     const { data, error: err } = await supabase
         .from("inventory")
-        .insert([{ ...input, amount: +input.amount, user: session?.user?.id }])
+        .insert(insert)
         .eq("user", session?.user?.id)
         .select()
-        .single()
 
     if (err) {
         console.log(err)
         throw error(500, { message: `Erro ao inserir produto no estoque - ${err?.message}` })
     }
-
-    return json(data, { status: 201, statusText: "Entrada de produto registrada com sucesso." })
+    return json(data[0], { status: 201, statusText: "Entrada de produto registrada com sucesso." })
 }
 
 export const DELETE: RequestHandler = async ({ locals: { supabase, getSession }, request }) => {
