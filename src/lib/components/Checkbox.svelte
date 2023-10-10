1e2894d6e06d21c7c981fcd7176db8c2a32fc2c2
Bruno Prado <bwprado@gmail.com>
2023-10-10 08:35:33 -0300

    🏷️ Updates `value` type

--- src/lib/components/Checkbox.svelte
+++ src/lib/components/Checkbox.svelte
@@ -8,11 +8,10 @@
         label?: string
         input?: string
     } = {}
-    export let type: HTMLInputElement["type"] = "checkbox"
     export let name: string = "input"
     export let id: string = "input"
     export let required: boolean = false
-    export let value: string = ""
+    export let value: any = undefined
     export let disabled: boolean = false
     export let selected: boolean = false
     export let message: string = ""
