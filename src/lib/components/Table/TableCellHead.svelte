8f3c8ed535f79c50bedcd4a7cdc27c459c32e2a2
Bruno Prado <bwprado@gmail.com>
2023-11-12 22:49:08 -0300

    Update TableCellHead alignment

--- src/lib/components/Table/TableCellHead.svelte
+++ src/lib/components/Table/TableCellHead.svelte
@@ -2,6 +2,6 @@
     import { twMerge } from "tailwind-merge"
 </script>
 
-<th scope="row" class={twMerge("p-4", "font-bold whitespace-nowrap table-cell text-xs align-top")}>
+<th scope="row" class={twMerge("p-4", "font-bold whitespace-nowrap table-cell text-xs !align-middle")}>
     <slot />
 </th>
