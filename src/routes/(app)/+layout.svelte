28d3aa6db0b50e719773c63da494bceb9b931d30
Bruno Prado <bwprado@gmail.com>
2023-11-12 22:49:19 -0300

    Add custom modal for editing output in app layout.

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,16 +1,23 @@
 <script lang="ts">
     import { page } from "$app/stores"
     import { arrow, autoUpdate, computePosition, flip, offset, shift } from "@floating-ui/dom"
-    import { AppShell, Modal, Toast, initializeStores, storePopup, type ModalComponent } from "@skeletonlabs/skeleton"
+    import {
+        AppShell,
+        Modal,
+        Toast,
+        initializeStores,
+        storePopup,
+        type ModalComponent,
+    } from "@skeletonlabs/skeleton"
     import "../../app.css"
     import type { LayoutData } from "./$types"
 
-    import Breadcrumbs from "$lib/components/Breadcrumbs.svelte"
+    import ModalEditOutput from "$lib/components/CustomModals/ModalEditOutput.svelte"
     import Footer from "$lib/components/Footer.svelte"
     import Header from "$lib/components/Header.svelte"
     import MenuItems from "$lib/components/MenuItems.svelte"
-    import Sidebar from "$lib/components/Sidebar.svelte"
     import ModalLoading from "$lib/components/ModalLoading.svelte"
+    import Sidebar from "$lib/components/Sidebar.svelte"
 
     storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow })
 
@@ -22,8 +29,11 @@
 
     const modalRegistry: Record<string, ModalComponent> = {
         modalComponentLoading: {
-            ref: ModalLoading
-        }
+            ref: ModalLoading,
+        },
+        modalEditOutput: {
+            ref: ModalEditOutput,
+        },
     }
 </script>
 
