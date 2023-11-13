87a10a259c5e1eca02f469057d7f67bd39d139e2
Bruno Prado <bwprado@gmail.com>
2023-11-12 22:49:04 -0300

    Update TableCell alignment

--- src/lib/components/Table/TableCell.svelte
+++ src/lib/components/Table/TableCell.svelte
@@ -4,6 +4,6 @@
     export let customClass: string = ""
 </script>
 
-<td class={twMerge("table-cell", customClass, "!px-4 align-top")}>
+<td class={twMerge("table-cell", customClass, "!px-4 !align-middle")}>
     <slot />
 </td>
