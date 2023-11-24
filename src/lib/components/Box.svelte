304dec0e63a98d72582f878524beab422e909cc9
Bruno Prado <bwprado@gmail.com>
2023-11-24 15:42:46 -0300

    Add Box component with title prop

---
+++ src/lib/components/Box.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    export let title: string = "Smart Stock It"
+</script>
+
+<div class="flex flex-col gap-y-4 rounded-lg dark:bg-surface-800 bg-surface-100 p-6 border dark:border-surface-500 border-success-300">
+    <h2 class="text-lg font-bold dark:text-white text-surface-900">{title}</h2>
+    <slot />
+</div>
