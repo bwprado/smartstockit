6dcbb20a2c220c56d064357231c1ab59e8d7f986
Bruno Prado <bwprado@gmail.com>
2023-11-25 05:30:27 -0300

    Reorder plugins in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { sveltekit } from "@sveltejs/kit/vite"
 import type { UserConfig } from "vite"
 
 const config: UserConfig = {
-    plugins: [sveltekit(), enhancedImages()],
+    plugins: [enhancedImages(), sveltekit()],
 }
 
 export default config
