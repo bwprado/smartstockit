bf2f7d786bd9c2f600f3c5a068fe71dda29d1dbe
Bruno Prado <bwprado@gmail.com>
2023-12-05 23:07:50 -0300

    Refactor login page layout and add hero image

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -2,7 +2,7 @@
     import { enhance } from "$app/forms"
     import { getModalStore } from "@skeletonlabs/skeleton"
     import type { SubmitFunction } from "@sveltejs/kit"
-    
+
     import Button from "$lib/components/Button.svelte"
     import Input from "$lib/components/Input.svelte"
 
@@ -20,38 +20,67 @@
 </script>
 
 <div
-    class="grid grid-rows-3 w-screen h-screen overflow-hidden items-center justify-center hero-gradient">
-    <div></div>
-    <div
-        class="w-[300px] sm:w-[500px] items-center flex flex-col gap-y-6 justify-center rounded-lg border dark:border-surface-500 bg-surface-800 p-10 sm:p-20">
+    class="hero-gradient grid h-screen w-screen grid-cols-1 items-center justify-center overflow-hidden lg:grid-cols-2">
+    <section class="hero-image hidden lg:block">
+        <div class="flex h-full flex-col items-center pb-10 pt-20">
+            <img
+                src="src\images\smartstockit-logo-primary.svg"
+                alt="SmartStockIt App"
+                class="mr-auto max-h-[300px] pl-10" />
+            <p class="mt-auto text-white">SmartStockIt App - 2023</p>
+        </div>
+    </section>
+    <div class="flex w-full flex-col items-center justify-center gap-y-6 rounded-lg p-20">
         <form
             method="post"
-            class="flex flex-col gap-y-4 items-center justify-center w-full"
+            class="flex max-w-lg flex-col items-center justify-center gap-y-4"
             use:enhance={handleSubmit}>
-            <h1 class="font-bold text-2xl">Smart Stock It</h1>
+            <img
+                src="src\images\smartstockit-logo-h-rasterized-primary.svg"
+                alt="SmartStockIt App"
+                class="w-full" />
             <p class="font-bold">Login</p>
             <Input id="email" name="email" label="Email" type="email" />
             <Input id="password" name="password" label="Password" type="password" />
             <Button type="submit" id="login">Login</Button>
             <div
-                class="grid grid-cols-[1fr,max-content,1fr] items-center justify-center w-full gap-x-4">
+                class="grid w-full grid-cols-[1fr,max-content,1fr] items-center justify-center gap-x-4">
                 <div class="border-b-[1px] border-surface-400" />
-                <p class="uppercase text-[10px] text-surface-400">ou continue com</p>
+                <p class="text-[10px] uppercase text-surface-400">ou continue com</p>
                 <div class="border-b-[1px] border-surface-400" />
             </div>
             <button
                 type="submit"
                 formaction="?provider=google"
-                class="flex items-center justify-center bg-surface-900 rounded-lg border border-scale-500 hover:bg-secondary-500/10 transition w-full h-10">
+                class="border-scale-500 flex h-10 w-full items-center justify-center rounded-lg border bg-surface-900 transition hover:bg-secondary-500/10">
                 <div
-                    class="relative flex items-center justify-center shrink-0 h-10 w-10 rounded-lg group overflow-hidden transition">
+                    class="group relative flex h-10 w-10 shrink-0 items-center justify-center overflow-hidden rounded-lg transition">
                     <img
                         class="w-5"
                         src="https://supabase.com/docs/img/icons/google-icon.svg"
                         alt="Google Icon" />
                 </div>
-                <h5 class="text-base text-scale-1200 px-4">Google</h5>
+                <h5 class="text-scale-1200 px-4 text-base">Google</h5>
             </button>
         </form>
     </div>
 </div>
+
+<style>
+    .hero-image {
+        background-image: image-set(
+            url("@images/hero-image-scale-w_1920.jpg") 1x,
+            url("@images/hero-image-scale-w_1831.jpg") 2x,
+            url("@images/hero-image-scale-w_1747.jpg") 3x,
+            url("@images/hero-image-scale-w_1568.jpg") 4x,
+            url("@images/hero-image-scale-w_1397.jpg") 5x,
+            url("@images/hero-image-scale-w_1176.jpg") 6x,
+            url("@images/hero-image-scale-w_1053.jpg") 7x,
+            url("@images/hero-image-scale-w_828.jpg") 8x,
+            url("@images/hero-image-scale-w_560.jpg") 9x
+        );
+        background-position: 10%;
+        background-size: cover;
+        height: 100%;
+    }
+</style>
